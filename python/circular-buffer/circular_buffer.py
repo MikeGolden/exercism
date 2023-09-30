15bbf3b3c39063c5aaffa74f927a5b621ac3d766
Mike Goldenberg <csrutil@tuta.io>
2023-09-30 17:09:05 +0200

    circular buffer solution

--- python/circular-buffer/circular_buffer.py
+++ python/circular-buffer/circular_buffer.py
@@ -20,16 +20,37 @@ class BufferEmptyException(BufferError):
 
 class CircularBuffer:
     def __init__(self, capacity):
-        pass
+        self.buffer = [None] * capacity
+        self.read_index = 0
+        self.write_index = 0
 
     def read(self):
-        pass
+        data = self.buffer[self.read_index]
+
+        if not data:
+            raise BufferEmptyException("Circular buffer is empty")
+
+        self.buffer[self.read_index] = None
+        self.read_index = (self.read_index + 1) % len(self.buffer)
+
+        return data
 
     def write(self, data):
-        pass
+        if self.buffer[self.write_index]:
+            raise BufferFullException("Circular buffer is full")
+
+        self.buffer[self.write_index] = data
+        self.write_index = (self.write_index + 1) % len(self.buffer)
 
     def overwrite(self, data):
-        pass
+        if self.buffer[self.write_index]:
+            self.buffer[self.read_index] = data
+            self.read_index = (self.read_index + 1) % len(self.buffer)
+            self.write_index = self.read_index
+        else:
+            self.write(data)
 
     def clear(self):
-        pass
+        self.buffer = [None] * len(self.buffer)
+        self.read_index = 0
+        self.write_index = 0
