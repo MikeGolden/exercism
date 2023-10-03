57dd66d214eba584aa84c4c066b9686429a7c788
Mike Goldenberg <csrutil@tuta.io>
2023-10-03 17:35:46 +0200

    simple cipher comments

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -3,6 +3,12 @@ import string
 
 class Cipher:
     def __init__(self, key=None):
+        """
+        Initialize a Cipher object.
+
+        Args:
+            key (str): The encryption key. If None, a random key of at least 100 lowercase characters will be generated.
+        """
         if key:
             if not key.isalpha() or not key.islower():
                 raise ValueError("Invalid key. Key must contain only lowercase letters.")
@@ -11,6 +17,15 @@ class Cipher:
             self.key = ''.join(random.choice(string.ascii_lowercase) for _ in range(100))
 
     def encode(self, text):
+        """
+        Encode a message using the Caesar Cipher.
+
+        Args:
+            text (str): The message to be encoded.
+
+        Returns:
+            str: The encoded message.
+        """
         result = ''
         for i, char in enumerate(text):
             shift = ord(self.key[i % len(self.key)]) - ord('a')
@@ -19,6 +34,15 @@ class Cipher:
         return result
 
     def decode(self, text):
+        """
+        Decode a message encoded with the Caesar Cipher.
+
+        Args:
+            text (str): The encoded message to be decoded.
+
+        Returns:
+            str: The decoded message.
+        """
         result = ''
         for i, char in enumerate(text):
             shift = ord(self.key[i % len(self.key)]) - ord('a')
