540d536177d2f394486a1d5e91f92a90ac6096fe
Mike Goldenberg <csrutil@tuta.io>
2023-10-03 18:01:20 +0200

    clock solution

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,18 +1,32 @@
 class Clock:
     def __init__(self, hour, minute):
-        pass
+        self.hour = hour
+        self.minute = minute
+        self.normalize_time()
 
     def __repr__(self):
-        pass
+        return f'Clock({self.hour}, {self.minute})'
 
     def __str__(self):
-        pass
+        return f'{self.format_digit(self.hour)}:{self.format_digit(self.minute)}'
 
     def __eq__(self, other):
-        pass
+        return self.hour == other.hour and self.minute == other.minute
 
     def __add__(self, minutes):
-        pass
+        self.minute += minutes
+        self.normalize_time()
+        return self
 
     def __sub__(self, minutes):
-        pass
+        self.minute -= minutes
+        self.normalize_time()
+        return self
+
+    def normalize_time(self):
+        self.hour += self.minute // 60
+        self.minute %= 60
+        self.hour %= 24
+
+    def format_digit(self, value):
+        return str(value).zfill(2)
