b170c2da3938fcd3c452dc5e81be0fd4f8ed8bb6
Mike Goldenberg <csrutil@tuta.io>
2023-10-03 17:34:15 +0200

    simple cipher solution

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,9 +1,27 @@
+import random
+import string
+
 class Cipher:
     def __init__(self, key=None):
-        pass
+        if key:
+            if not key.isalpha() or not key.islower():
+                raise ValueError("Invalid key. Key must contain only lowercase letters.")
+            self.key = key
+        else:
+            self.key = ''.join(random.choice(string.ascii_lowercase) for _ in range(100))
 
     def encode(self, text):
-        pass
+        result = ''
+        for i, char in enumerate(text):
+            shift = ord(self.key[i % len(self.key)]) - ord('a')
+            encoded_char = chr(((ord(char) - ord('a') + shift) % 26) + ord('a'))
+            result += encoded_char
+        return result
 
     def decode(self, text):
-        pass
+        result = ''
+        for i, char in enumerate(text):
+            shift = ord(self.key[i % len(self.key)]) - ord('a')
+            decoded_char = chr(((ord(char) - ord('a') - shift) % 26) + ord('a'))
+            result += decoded_char
+        return result
