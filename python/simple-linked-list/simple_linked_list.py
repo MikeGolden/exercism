690e29da98dec2eff4a41133bfeb4c2b19d263e2
Mike Goldenberg <csrutil@tuta.io>
2023-10-06 13:28:12 +0200

    simple linked list solution

--- python/simple-linked-list/simple_linked_list.py
+++ python/simple-linked-list/simple_linked_list.py
@@ -1,32 +1,62 @@
 class Node:
+
     def __init__(self, value):
-        pass
+        self._value = value
+        self._next = None
 
     def value(self):
-        pass
+        return self._value
 
     def next(self):
-        pass
+        return self._next
 
 
 class LinkedList:
+
     def __init__(self, values=[]):
-        pass
+        self._head = None
+        self._len = 0
+
+        for value in values:
+            self.push(value)
+
+    def __iter__(self):
+        items = []
+        curr = self._head
+
+        while curr:
+            items.append(curr.value())
+            curr = curr.next()
+
+        return iter(items)
 
     def __len__(self):
-        pass
+        return self._len
 
     def head(self):
-        pass
+        if not self._head:
+            raise EmptyListException("The list is empty.")
+
+        return self._head
 
     def push(self, value):
-        pass
+        node = Node(value)
+        node._next = self._head
+        self._head = node
+        self._len += 1
 
     def pop(self):
-        pass
+        if not self._head:
+            raise EmptyListException("The list is empty.")
+
+        value = self._head.value()
+        self._head = self._head.next()
+        self._len -= 1
+
+        return value
 
     def reversed(self):
-        pass
+        return LinkedList(self)
 
 
 class EmptyListException(Exception):
