737189d8235ec8b7a42574f3adeef0f3f9e71eea
Mike Goldenberg <csrutil@tuta.io>
2023-10-09 18:36:27 +0200

    zipper solution

--- python/zipper/zipper.py
+++ python/zipper/zipper.py
@@ -1,28 +1,55 @@
 class Zipper:
+    def __init__(self, value, parent):
+        self._value = value
+        self._parent = parent
+        self._root = None
+        self._left = None
+        self._right = None
+
     @staticmethod
-    def from_tree(tree):
-        pass
+    def from_tree(tree, parent=None, root=None):
+        if tree is None:
+            return None
+
+        node = Zipper(tree["value"], parent)
+        if root is None:
+            root = node
+        node._root = root
+        node._left = Zipper.from_tree(tree["left"], node, root)
+        node._right = Zipper.from_tree(tree["right"], node, root)
+
+        return node
 
     def value(self):
-        pass
+        return self._value
 
-    def set_value(self):
-        pass
+    def set_value(self, value):
+        self._value = value
+        return self
 
     def left(self):
-        pass
+        return self._left
 
-    def set_left(self):
-        pass
+    def set_left(self, tree):
+        self._left = Zipper.from_tree(tree, self, self._root)
+        return self
 
     def right(self):
-        pass
+        return self._right
 
-    def set_right(self):
-        pass
+    def set_right(self, tree):
+        self._right = Zipper.from_tree(tree, self, self._root)
+        return self
 
     def up(self):
-        pass
+        return self._parent
 
     def to_tree(self):
-        pass
+        return self._root.to_subtree()
+
+    def to_subtree(self):
+        return {
+            "value": self._value,
+            "left": self._left.to_subtree() if self._left else None,
+            "right": self._right.to_subtree() if self._right else None
+        }
