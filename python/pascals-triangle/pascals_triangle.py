a31316b320f8c9208240b21d52a9461e170b2077
Mike Goldenberg <csrutil@tuta.io>
2023-10-09 15:56:26 +0200

    pascals triangle correct solution

--- python/pascals-triangle/pascals_triangle.py
+++ python/pascals-triangle/pascals_triangle.py
@@ -1,24 +1,10 @@
-def rows(row_count):
-    # Check for the base case when row_count is 0
+def rows(row_count: int) -> list[list[int]]:
+    if row_count < 0:
+        raise ValueError("number of rows is negative")
     if row_count == 0:
         return []
-
-    # Recursive case: Generate the previous row
-    prev_triangle = rows(row_count - 1)
-
-    # Calculate the current row based on the previous row
-    current_row = [1]  # The first element is always 1
-    for i in range(1, row_count):
-        current_element = prev_triangle[row_count - 1][i - 1] + prev_triangle[row_count - 1][i]
-        current_row.append(current_element)
-
-    current_row.append(1)  # The last element is always 1
-
-    # Append the current row to the previous triangle
-    prev_triangle.append(current_row)
-
-    return prev_triangle
-
-def generate_pascals_triangle_rows(row_count):
-    triangle = rows(row_count)
-    return triangle
+    if row_count == 1:
+        return [[1]]
+    *prefix, last = rows(row_count-1)
+    new = [a+b for a,b in zip(last + [0], [0] + last)]
+    return [*prefix, last, new]
