525dd591a8f3f5d4b9a84ea0ea165829e7066b43
Mike Goldenberg <csrutil@tuta.io>
2023-10-09 15:36:21 +0200

    meetup correct solution

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,41 +1,38 @@
-# subclassing the built-in ValueError to create MeetupDayException
-from datetime import date, timedelta
+from calendar import day_name, monthrange, weekday
+from datetime import date
 
-class MeetupDayException(ValueError):
-    """Exception raised when the Meetup weekday and count do not result in a valid date.
 
-    message: explanation of the error.
+class MeetupDayException(Exception):
+    pass
 
-    """
-    def __init__(self):
-        pass
 
-def meetup(year, month, week, day_of_week):
-    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+START_DAY = {
+    'first': 1,
+    'second': 8,
+    'third': 15,
+    'fourth': 22,
+    'fifth': 29,
+    'teenth': 13,
+    'last': None
+}
 
-    if week == 'teenth':
-        day = 13
-        while True:
-            meetup_date = date(year, month, day)
-            if weekdays[meetup_date.weekday()] == day_of_week:
-                return meetup_date
-            day += 1
 
-    count = 0
-    meetup_date = date(year, month, 1)
+def meetup(year, month, which, week_day):
+    if which not in START_DAY:
+        raise MeetupDayException(f'What is {which}?')
 
-    while count < 4:
-        if weekdays[meetup_date.weekday()] == day_of_week:
-            count += 1
-            if count == week:
-                return meetup_date
-        meetup_date += timedelta(days=1)
+    if week_day not in day_name:
+        raise MeetupDayException(f'Unknown day: {week_day}')
 
-    if week == 'last':
-        while meetup_date.month == month:
-            last_meetup_date = meetup_date
-            meetup_date += timedelta(days=1)
-            if meetup_date.month != month:
-                return last_meetup_date
+    start_day = START_DAY[which] or monthrange(year, month)[1] - 6
 
-    raise MeetupDayException()
+    # Both calendar.weekday() and datetime.date() can throw
+    # ValueError: day is out of range for month
+    try:
+        start_day_wday = weekday(year, month, start_day)
+        wanted_wday = list(day_name).index(week_day)
+        delta = (wanted_wday - start_day_wday) % 7
+        return date(year, month, start_day + delta)
+
+    except ValueError:
+        raise MeetupDayException("That day does not exist.")
