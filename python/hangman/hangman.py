0263dc8b3a992ec43f97133e7a80d78e1fcf15b8
Mike Goldenberg <csrutil@tuta.io>
2023-10-09 16:34:23 +0200

    hangman right solution

--- python/hangman/hangman.py
+++ python/hangman/hangman.py
@@ -3,41 +3,39 @@ STATUS_WIN = 'win'
 STATUS_LOSE = 'lose'
 STATUS_ONGOING = 'ongoing'
 
-
 class Hangman:
     def __init__(self, word):
-        self.word = word
+        # Initialize the game with 9 remaining guesses and an ongoing status.
         self.remaining_guesses = 9
-        self.guessed_chars = set()
         self.status = STATUS_ONGOING
+        self.word = word  # The word to be guessed.
+        self.masked_word = ["_"] * len(word)  # Initialize a masked word with underscores.
 
     def guess(self, char):
+        # Check if the game has already ended, and if so, raise an error.
         if self.status != STATUS_ONGOING:
             raise ValueError("The game has already ended.")
 
-        char = char.lower()
-        if char in self.guessed_chars:
-            return
+        if char in self.word and char not in self.masked_word:
+            # If the guessed character is in the word and not already guessed:
+            for i in range(len(self.word)):
+                if self.word[i] == char:
+                    self.masked_word[i] = char  # Reveal the character in the masked word.
 
-        self.guessed_chars.add(char)
-
-        if char not in self.word:
-            self.remaining_guesses -= 1
-
-        if self.remaining_guesses < 0:
-            self.status = STATUS_LOSE
-
-        if set(self.word).issubset(self.guessed_chars):
-            self.status = STATUS_WIN
+            if all(c != "_" for c in self.masked_word):
+                # If there are no underscores left in the masked word, the player has won.
+                self.status = STATUS_WIN
+        else:
+            if self.remaining_guesses == 0:
+                # If there are no remaining guesses left, the player has lost.
+                self.status = STATUS_LOSE
+            else:
+                self.remaining_guesses -= 1  # Decrement the remaining guesses.
 
     def get_masked_word(self):
-        masked_word = ''
-        for char in self.word:
-            if char.lower() in self.guessed_chars:
-                masked_word += char
-            else:
-                masked_word += '_'
-        return masked_word
+        # Get the current state of the masked word as a string.
+        return "".join(self.masked_word)
 
     def get_status(self):
+        # Get the current game status (win, lose, or ongoing).
         return self.status
