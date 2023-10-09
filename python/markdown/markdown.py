0ef8f6d80de3cbd9b7117d4ab86a5b87ef25d43c
Mike Goldenberg <csrutil@tuta.io>
2023-10-09 15:16:33 +0200

    markdown solution

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -2,76 +2,51 @@ import re
 
 
 def parse(markdown):
-    lines = markdown.split('\n')
-    res = ''
-    in_list = False
-    in_list_append = False
-    for i in lines:
-        if re.match('###### (.*)', i) is not None:
-            i = '<h6>' + i[7:] + '</h6>'
-        elif re.match('##### (.*)', i) is not None:
-            i = '<h5>' + i[6:] + '</h5>'
-        elif re.match('#### (.*)', i) is not None:
-            i = '<h4>' + i[5:] + '</h4>'
-        elif re.match('### (.*)', i) is not None:
-            i = '<h3>' + i[4:] + '</h3>'
-        elif re.match('## (.*)', i) is not None:
-            i = '<h2>' + i[3:] + '</h2>'
-        elif re.match('# (.*)', i) is not None:
-            i = '<h1>' + i[2:] + '</h1>'
-        m = re.match(r'\* (.*)', i)
-        if m:
-            if not in_list:
-                in_list = True
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                    is_italic = True
-                i = '<ul><li>' + curr + '</li>'
-            else:
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    is_italic = True
-                if is_bold:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                if is_italic:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                i = '<li>' + curr + '</li>'
-        else:
-            if in_list:
-                in_list_append = True
-                in_list = False
+    start_list = False
+    lines = [parse_line(line) for line in markdown.splitlines()]
 
-        m = re.match('<h|<ul|<p|<li', i)
-        if not m:
-            i = '<p>' + i + '</p>'
-        m = re.match('(.*)__(.*)__(.*)', i)
-        if m:
-            i = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
-        m = re.match('(.*)_(.*)_(.*)', i)
-        if m:
-            i = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
-        if in_list_append:
-            i = '</ul>' + i
-            in_list_append = False
-        res += i
-    if in_list:
-        res += '</ul>'
-    return res
+    for i in range(len(lines)):
+        if not start_list and re.match(r'<li>', lines[i]):
+            lines[i] = '<ul>' + lines[i]
+            start_list = True
+        elif start_list and (i == len(lines) - 1 or not re.match(r'<li>', lines[i + 1])):
+            lines[i] += '</ul>'
+            start_list = False
+
+    return ''.join(lines)
+
+
+def parse_line(markdown):
+    markdown = emphasis(markdown)
+
+    if is_header(markdown):
+        i = markdown.index(' ')
+        return '<h{0}>{1}</h{0}>'.format(i, markdown[i + 1:])
+    elif is_unordered_list(markdown):
+        return '<li>{}</li>'.format(markdown[2:])
+    else:
+        return '<p>{}</p>'.format(markdown)
+
+
+def emphasis(markdown):
+    while re.match(r'(.*)__(.*)__(.*)', markdown):
+        i = markdown.index('__')
+        j = markdown.rindex('__')
+        markdown = '{}<strong>{}</strong>{}'.format(
+            markdown[:i], markdown[i + 2:j], markdown[j + 2:])
+
+    while re.match(r'(.*)_(.*)_(.*)', markdown):
+        i = markdown.index('_')
+        j = markdown.rindex('_')
+        markdown = '{}<em>{}</em>{}'.format(
+            markdown[:i], markdown[i + 1:j], markdown[j + 1:])
+
+    return markdown
+
+
+def is_header(markdown):
+    return re.match(r'#{1,6} (.+)', markdown)
+
+
+def is_unordered_list(markdown):
+    return re.match(r'\* (.*)', markdown)
