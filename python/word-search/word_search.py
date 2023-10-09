4708c25c7e75c1b899ef93d0a54fa202935e93a5
Mike Goldenberg <csrutil@tuta.io>
2023-10-09 17:57:40 +0200

    word search right solution

--- python/word-search/word_search.py
+++ python/word-search/word_search.py
@@ -1,50 +1,60 @@
 class Point:
     def __init__(self, x, y):
-        self.x = x  # Initialize the x-coordinate of the Point object
-        self.y = y  # Initialize the y-coordinate of the Point object
+        self.x = x
+        self.y = y
 
     def __eq__(self, other):
-        # Define the equality comparison method for Point objects
         return self.x == other.x and self.y == other.y
 
 
 class WordSearch:
     def __init__(self, puzzle):
-        self.puzzle = puzzle
+        self.sizey = len(puzzle)
+        self.sizex = len(puzzle[0])
+        self.horizontal = puzzle
+        self.vertical = []
+        self.diagonal_l2r = []
+        self.diagonal_r2l = []
 
-    def search(self, word):
-        word = word.lower()  # Convert the word to lowercase for case-insensitive search
+        for x in range(self.sizex):
+            self.vertical.append(
+                "".join(puzzle[y][x] for y in range(self.sizey)))
+        for x in range(1 - self.sizey, self.sizex):
+            y = max(-x, 0)
+            x = max(x, 0)
+            self.diagonal_l2r.append("".join(
+                puzzle[y + i][x + i]
+                for i in range(min(self.sizey - y, self.sizex - x))))
+            self.diagonal_r2l.append("".join(
+                puzzle[y + i][-x - i - 1]
+                for i in range(min(self.sizey - y, self.sizex - x))))
 
-        # Define possible directions (horizontal, vertical, diagonal, and reverse diagonal)
-        directions = [
-            (0, 1),  # Right
-            (1, 0),  # Down
-            (1, 1),  # Diagonal down-right
-            (-1, 1),  # Diagonal down-left
-        ]
+    def search(self, word, reverse=False):
+        for y in range(self.sizey):
+            if word in self.horizontal[y]:
+                x = self.horizontal[y].find(word)
+                return (Point(x, y), Point(x + len(word) - 1, y))
+        for x in range(self.sizex):
+            if word in self.vertical[x]:
+                y = self.vertical[x].find(word)
+                return (Point(x, y), Point(x, y + len(word) - 1))
+        for z in range(len(self.diagonal_l2r)):
+            if word in self.diagonal_l2r[z]:
+                i = self.diagonal_l2r[z].find(word)
+                x = z - self.sizey + 1
+                y = max(-x, 0) + i
+                x = max(x, 0) + i
+                return (Point(x, y), Point(x + len(word) - 1, y + len(word) - 1))
+            elif word in self.diagonal_r2l[z]:
+                i = self.diagonal_r2l[z].find(word)
+                x = z - self.sizey + 1
+                y = max(-x, 0) + i
+                x = self.sizex - 1 - max(x, 0) - i
+                return (Point(x, y), Point(x - len(word) + 1, y + len(word) - 1))
 
-        for row in range(len(self.puzzle)):
-            for col in range(len(self.puzzle[row])):
-                for dx, dy in directions:
-                    x, y = col, row  # Initialize starting point
-                    found = True  # Assume the word is found
-                    for letter in word:
-                        if (
-                            x < 0
-                            or x >= len(self.puzzle[0])
-                            or y < 0
-                            or y >= len(self.puzzle)
-                            or self.puzzle[y][x].lower() != letter
-                        ):
-                            found = False  # Word not found
-                            break
-                        x += dx  # Move in the current direction
-                        y += dy
-                    if found:
-                        # Calculate the starting and ending points of the word
-                        start = Point(col, row)
-                        end = Point(x - dx, y - dy)
-                        return start, end  # Return the first occurrence
+        if not reverse:
+            search_rev = self.search(word[::-1], True)
+            if search_rev is not None:
+                return (search_rev[1], search_rev[0])
 
-        # Return None if the word is not found
         return None
