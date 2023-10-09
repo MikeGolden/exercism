737cb395eaa19907188a8b89b8be2369207598b5
Mike Goldenberg <csrutil@tuta.io>
2023-10-09 17:58:57 +0200

    word search comments

--- python/word-search/word_search.py
+++ python/word-search/word_search.py
@@ -1,3 +1,4 @@
+# Define a Point class to represent coordinates (x, y)
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -6,9 +7,10 @@ class Point:
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
-
+# Define the WordSearch class
 class WordSearch:
     def __init__(self, puzzle):
+        # Initialize puzzle size and create lists for horizontal, vertical, diagonal left-to-right, and diagonal right-to-left words
         self.sizey = len(puzzle)
         self.sizex = len(puzzle[0])
         self.horizontal = puzzle
@@ -16,6 +18,7 @@ class WordSearch:
         self.diagonal_l2r = []
         self.diagonal_r2l = []
 
+        # Create vertical and diagonal words lists based on the puzzle
         for x in range(self.sizex):
             self.vertical.append(
                 "".join(puzzle[y][x] for y in range(self.sizey)))
@@ -29,15 +32,21 @@ class WordSearch:
                 puzzle[y + i][-x - i - 1]
                 for i in range(min(self.sizey - y, self.sizex - x))))
 
+    # Define the search function to find a word in the puzzle
     def search(self, word, reverse=False):
+        # Search for the word in horizontal rows
         for y in range(self.sizey):
             if word in self.horizontal[y]:
                 x = self.horizontal[y].find(word)
                 return (Point(x, y), Point(x + len(word) - 1, y))
+
+        # Search for the word in vertical columns
         for x in range(self.sizex):
             if word in self.vertical[x]:
                 y = self.vertical[x].find(word)
                 return (Point(x, y), Point(x, y + len(word) - 1))
+
+        # Search for the word in diagonal left-to-right and right-to-left directions
         for z in range(len(self.diagonal_l2r)):
             if word in self.diagonal_l2r[z]:
                 i = self.diagonal_l2r[z].find(word)
@@ -52,9 +61,11 @@ class WordSearch:
                 x = self.sizex - 1 - max(x, 0) - i
                 return (Point(x, y), Point(x - len(word) + 1, y + len(word) - 1))
 
+        # If the word is not found and reverse is not True, try searching in reverse
         if not reverse:
             search_rev = self.search(word[::-1], True)
             if search_rev is not None:
                 return (search_rev[1], search_rev[0])
 
+        # Return None if the word is not found
         return None
