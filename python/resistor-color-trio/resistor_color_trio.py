a3e8da861d03e1e576597bea4ed7c4acb92d44cf
Mike Goldenberg <csrutil@tuta.io>
2023-10-09 16:36:43 +0200

    resistor color trio right solution

--- python/resistor-color-trio/resistor_color_trio.py
+++ python/resistor-color-trio/resistor_color_trio.py
@@ -1,20 +1,29 @@
+resistors = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
+
+metric_prefix = {1000000000: "giga", 1000000: "mega", 1000: "kilo"}
+
+
 def label(colors):
-    adict = {
-        "black": 0,
-        "brown": 1,
-        "red": 2,
-        "orange": 3,
-        "yellow": 4,
-        "green": 5,
-        "blue": 6,
-        "violet": 7,
-        "grey": 8,
-        "white": 9,
-    }
-    result = ""
-    for color in colors:
-        result += str(adict[color])
-    result = int(result[:-1]) * 10 ** int(result[-1])
-    if result >= 1000:
-        return f"{result // 1000} kiloohms"
-    return f"{result} ohms"
+    res = [resistors[v] for v in colors]
+    resistance = int(f"{res[0]}{res[1]}")
+    magnitude = int(f"{resistance}{'0'*res[2]}")
+    suffix = "ohms"
+
+    for base, prefix in metric_prefix.items():
+        if magnitude >= base:
+            suffix = f"{prefix}{suffix}"
+            magnitude = int(magnitude / base)
+            break
+
+    return f"{magnitude} {suffix}"
