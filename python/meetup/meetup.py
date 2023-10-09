f7d9d8f345f1608c98e0f3c5d9a5dcbb973494f0
Mike Goldenberg <csrutil@tuta.io>
2023-10-09 15:37:49 +0200

    meetup comments

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,11 +1,12 @@
+# Import necessary modules from the Python standard library
 from calendar import day_name, monthrange, weekday
 from datetime import date
 
-
+# Define a custom exception class for MeetupDayException
 class MeetupDayException(Exception):
     pass
 
-
+# Define a dictionary to map week descriptors to their starting days
 START_DAY = {
     'first': 1,
     'second': 8,
@@ -16,23 +17,29 @@ START_DAY = {
     'last': None
 }
 
-
+# Define the meetup function
 def meetup(year, month, which, week_day):
+    # Check if the provided week descriptor is valid
     if which not in START_DAY:
         raise MeetupDayException(f'What is {which}?')
 
+    # Check if the provided week day is valid
     if week_day not in day_name:
         raise MeetupDayException(f'Unknown day: {week_day}')
 
+    # Determine the starting day of the meetup based on the week descriptor
     start_day = START_DAY[which] or monthrange(year, month)[1] - 6
 
-    # Both calendar.weekday() and datetime.date() can throw
-    # ValueError: day is out of range for month
     try:
+        # Calculate the weekday (0 = Monday, 1 = Tuesday, etc.) of the starting day
         start_day_wday = weekday(year, month, start_day)
+        # Calculate the desired weekday (0 = Monday, 1 = Tuesday, etc.)
         wanted_wday = list(day_name).index(week_day)
+        # Calculate the delta between the desired weekday and starting day's weekday
         delta = (wanted_wday - start_day_wday) % 7
+        # Calculate the meetup date by adding the delta to the starting day
         return date(year, month, start_day + delta)
 
     except ValueError:
+        # Handle the case where the date is invalid, such as February 30
         raise MeetupDayException("That day does not exist.")
