064ff821ba1601f9da99a0d42d30aa9a334cafc2
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 13:09:21 +0200

    largest series product solution

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,2 +1,22 @@
 def largest_product(series, size):
-    pass
+    if size < 0:
+        raise ValueError("span must not be negative")
+
+    if size > len(series):
+        raise ValueError("span must be smaller than string length")
+
+    if not series.isdigit():
+        raise ValueError("digits input must only contain digits")
+
+    max_product = 0
+
+    for i in range(len(series) - size + 1):
+        sub_series = series[i:i + size]
+        product = 1
+
+        for digit in sub_series:
+            product *= int(digit)
+
+        max_product = max(max_product, product)
+
+    return max_product
