9aa2f069a4b502635efc7f40c80ebdd28cc0bab1
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 18:33:01 +0200

    pov solution

--- python/pov/pov.py
+++ python/pov/pov.py
@@ -19,7 +19,49 @@ class Tree:
         return self.__dict__() == other.__dict__()
 
     def from_pov(self, from_node):
-        pass
+        return self.clone().from_pov_helper(from_node)
 
     def path_to(self, from_node, to_node):
-        pass
+        path = self.from_pov(from_node).path_to_helper(to_node)
+        if path is None:
+            raise ValueError("No path found")
+        
+        return path
+    
+    def clone(self):
+        return Tree(self.label, [c.clone() for c in self.children])
+    
+    def find(self, label):
+        nodes = [(None, self)]
+
+        while nodes != []:
+            parent, curr = nodes.pop()
+            if curr.label == label:
+                return (parent, curr)
+            nodes.extend((curr, c) for c in curr.children)
+
+        raise ValueError("Tree could not be reoriented")
+    
+    def from_pov_helper(self, from_node):
+        parent, from_node = self.find(from_node)
+
+        if parent is not None:
+            for i in range(len(parent.children)):
+                if parent.children[i].label == from_node.label:
+                    parent.children.pop(i)
+                    break
+            parent = self.from_pov_helper(parent.label)
+            from_node.children.append(parent)
+        
+        return from_node
+
+    def path_to_helper(self, to_node):
+        if self.label == to_node:
+            return [self.label]
+        
+        for c in self.children:
+            path = c.path_to_helper(to_node)
+            if path is not None:
+                return [self.label] + path
+        
+        return None
