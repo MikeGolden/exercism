ea9ff9b930a9d209057427d8839c3186cb45df29
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 17:21:06 +0200

    killer sudoku helper solution

--- python/killer-sudoku-helper/killer_sudoku_helper.py
+++ python/killer-sudoku-helper/killer_sudoku_helper.py
@@ -1,2 +1,31 @@
-def combinations(target, size, exclude):
-    pass
+from itertools import combinations as it_combinations
+
+def combinations(target: int, size: int, exclude: set = None) -> list:
+    """
+    Generate all valid combinations for a given cage in Killer Sudoku.
+
+    :param target: The target sum for the cage.
+    :param size: The size of the cage.
+    :param exclude: A set of digits to exclude from the combinations.
+    :return: A list of valid combinations for the cage.
+    """
+    if exclude is None:
+        exclude = set()
+
+    def backtrack(start, current_combination):
+        if len(current_combination) == size and sum(current_combination) == target:
+            result.append(tuple(sorted(current_combination)))
+            return
+
+        if len(current_combination) >= size or sum(current_combination) > target:
+            return
+
+        for digit in range(start, 10):
+            if digit not in exclude:
+                current_combination.append(digit)
+                backtrack(digit + 1, current_combination)
+                current_combination.pop()
+
+    result = []
+    backtrack(1, [])
+    return sorted(result)
