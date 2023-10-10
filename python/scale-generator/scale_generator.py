014fecbfffff050b4d3de2f4b8f184b1c1689b20
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 13:02:59 +0200

    scale generator solution

--- python/scale-generator/scale_generator.py
+++ python/scale-generator/scale_generator.py
@@ -1,9 +1,31 @@
 class Scale:
     def __init__(self, tonic):
-        pass
+        if tonic in ["F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"]:
+            notes = ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"]
+        else:
+            notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+
+        i = notes.index(tonic.capitalize())
+        self.notes = notes[i:] + notes[:i]
 
     def chromatic(self):
-        pass
+        return self.notes
 
     def interval(self, intervals):
-        pass
+        i = 0
+        ret = []
+
+        for step in intervals:
+            ret.append(self.notes[i])
+
+            if step == 'm':
+                i += 1
+            elif step == 'M':
+                i += 2
+            elif step == 'A':
+                i += 3
+
+            i %= len(self.notes)  # Ensure it wraps around the scale
+
+        return ret
+
