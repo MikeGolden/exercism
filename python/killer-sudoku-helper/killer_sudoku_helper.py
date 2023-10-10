68751b3629ae8b2fb8cee8fc42a35e0652923270
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 17:23:17 +0200

    killer sudoku helper right solution

--- python/killer-sudoku-helper/killer_sudoku_helper.py
+++ python/killer-sudoku-helper/killer_sudoku_helper.py
@@ -1,31 +1,25 @@
-from itertools import combinations as it_combinations
+from typing import List
 
-def combinations(target: int, size: int, exclude: set = None) -> list:
+def combinations(target: int, size: int, exclude: List[int]) -> List[List[int]]:
     """
     Generate all valid combinations for a given cage in Killer Sudoku.
 
     :param target: The target sum for the cage.
     :param size: The size of the cage.
-    :param exclude: A set of digits to exclude from the combinations.
+    :param exclude: A list of digits to exclude from the combinations.
     :return: A list of valid combinations for the cage.
     """
-    if exclude is None:
-        exclude = set()
-
-    def backtrack(start, current_combination):
-        if len(current_combination) == size and sum(current_combination) == target:
-            result.append(tuple(sorted(current_combination)))
-            return
-
-        if len(current_combination) >= size or sum(current_combination) > target:
-            return
-
-        for digit in range(start, 10):
-            if digit not in exclude:
-                current_combination.append(digit)
-                backtrack(digit + 1, current_combination)
-                current_combination.pop()
-
-    result = []
-    backtrack(1, [])
-    return sorted(result)
+    if size == 1:
+        if 1 <= target <= 9 and target not in exclude:
+            return [[target]]
+        else:
+            return []
+    else:
+        results = []
+        for n in range(1, 10):
+            if n not in exclude:
+                for c in combinations(target - n, size - 1, exclude + [n]):
+                    combination = sorted([n, *c])
+                    if combination not in results:
+                        results.append(combination)
+        return results
