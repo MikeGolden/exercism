f637fa53dd6cf477f9aca3f65020a4a6dbf14e65
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 13:21:51 +0200

    complex numbers solution

--- python/complex-numbers/complex_numbers.py
+++ python/complex-numbers/complex_numbers.py
@@ -1,27 +1,41 @@
+import math
+
 class ComplexNumber:
     def __init__(self, real, imaginary):
-        pass
+        self.real = real
+        self.imaginary = imaginary
 
     def __eq__(self, other):
-        pass
+        return self.real == other.real and self.imaginary == other.imaginary
 
     def __add__(self, other):
-        pass
+        real_part = self.real + other.real
+        imaginary_part = self.imaginary + other.imaginary
+        return ComplexNumber(real_part, imaginary_part)
 
     def __mul__(self, other):
-        pass
+        real_part = (self.real * other.real) - (self.imaginary * other.imaginary)
+        imaginary_part = (self.real * other.imaginary) + (self.imaginary * other.real)
+        return ComplexNumber(real_part, imaginary_part)
 
     def __sub__(self, other):
-        pass
+        real_part = self.real - other.real
+        imaginary_part = self.imaginary - other.imaginary
+        return ComplexNumber(real_part, imaginary_part)
 
     def __truediv__(self, other):
-        pass
+        denominator = (other.real ** 2) + (other.imaginary ** 2)
+        real_part = ((self.real * other.real) + (self.imaginary * other.imaginary)) / denominator
+        imaginary_part = ((self.imaginary * other.real) - (self.real * other.imaginary)) / denominator
+        return ComplexNumber(real_part, imaginary_part)
 
     def __abs__(self):
-        pass
+        return math.sqrt((self.real ** 2) + (self.imaginary ** 2))
 
     def conjugate(self):
-        pass
+        return ComplexNumber(self.real, -self.imaginary)
 
     def exp(self):
-        pass
+        real_part = math.exp(self.real) * math.cos(self.imaginary)
+        imaginary_part = math.exp(self.real) * math.sin(self.imaginary)
+        return ComplexNumber(real_part, imaginary_part)
