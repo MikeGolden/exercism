f846ca31c315d09e4470e6cbe1b6cdd0bb6bf248
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 17:52:39 +0200

    sgf parsing right solution

--- python/sgf-parsing/sgf_parsing.py
+++ python/sgf-parsing/sgf_parsing.py
@@ -2,90 +2,73 @@ class SgfTree:
     def __init__(self, properties=None, children=None):
         self.properties = properties or {}
         self.children = children or []
-        if any(not k.isupper() or v == [] for k, v in self.properties.items()):
-            raise ValueError("property must be in uppercase")
-
     def __eq__(self, other):
         if not isinstance(other, SgfTree):
             return False
-        for k, v in self.properties.items():
-            if k not in other.properties:
+        for key, value in self.properties.items():
+            if key not in other.properties:
                 return False
-            if other.properties[k] != v:
+            if other.properties[key] != value:
                 return False
-        for k in other.properties.keys():
-            if k not in self.properties:
+        for key in other.properties.keys():
+            if key not in self.properties:
                 return False
         if len(self.children) != len(other.children):
             return False
-        for a, b in zip(self.children, other.children):
-            if a != b:
+        for child, other_child in zip(self.children, other.children):
+            if child != other_child:
                 return False
         return True
-
     def __ne__(self, other):
         return not self == other
-
-
-def parse(input_string):
-    if not input_string.startswith("(;") or not input_string.endswith(")"):
-        raise ValueError("properties without delimiter")
-
-    input_string = input_string[2:-1].replace("\t", " ")
-    properties = {}
-    children = None
-    is_key = True
-    key = ""
-    val = ""
-    i = 0
-
-    while i < len(input_string):
-        if is_key and i + 1 == len(input_string):
-            properties[key] = []
-        elif is_key and input_string[i] == "[":
-            properties[key] = []
-            is_key = False
-        elif not is_key and input_string[i] == "]":
-            properties[key].append(val)
-            val = ""
-            if i + 1 < len(input_string) and input_string[i + 1] == "[":
-                i += 1
-            elif i + 1 < len(input_string) and input_string[i + 1] == ";":
-                children = [parse("(" + input_string[i + 1:] + ")")]
-                break
-            elif i + 1 < len(input_string) and input_string[i + 1] == "(":
-                children = parse_children(input_string[i + 1:])
-                break
-            else:
-                is_key = True
-                key = ""
-        elif not is_key and input_string[i:i + 2] == "\\]":
-            val += "]"
-            i += 1
-        elif is_key:
-            key += input_string[i]
-        else:
-            val += input_string[i]
-
-        i += 1
-
-    return SgfTree(properties, children)
-
-
-def parse_children(input_string):
-    count = 0
-    indices = []
-
-    for i in range(len(input_string)):
-        if input_string[i] == "(":
-            if count == 0:
-                indices.append(i)
-            count += 1
-        elif input_string[i] == ")":
-            count -= 1
-        if count < 0:
-            raise ValueError("tree with no nodes")
-    indices.append(len(input_string))
-
-    return [parse(input_string[indices[i]:indices[i + 1]])
-            for i in range(len(indices) - 1)]
+def parse(input):
+    pos = 1
+    def get_property():
+        nonlocal pos
+        ind = pos
+        while input[ind].isupper():
+            ind += 1
+        if input[ind].islower():
+            raise ValueError('property must be in uppercase')
+        if input[ind] != '[':
+            raise ValueError('properties without delimiter')
+        key = input[pos:ind]
+        prop = {key: []}
+        pos = ind
+        while input[pos] == '[':
+            pos = ind = pos + 1
+            w = []
+            while input[ind] != ']':
+                if input[ind] == '\t':
+                    w.append(' ')
+                elif input[ind:ind + 2] in ('\\]', '\\\\'):
+                    w.append(input[ind + 1])
+                    ind += 1
+                elif input[ind:ind + 2] == '\\\n':
+                    ind += 1
+                elif input[ind] != '\\':
+                    w.append(input[ind])
+                ind += 1
+            prop[key].append(''.join(w))
+            pos = ind + 1
+        return prop
+    
+    def get_node():
+        nonlocal pos
+        properties = {}
+        children = []
+        while input[pos] == '(':
+            pos += 1
+        if input[pos] == ')':
+            raise ValueError('tree with no nodes')
+        pos += 1
+        while input[pos] not in ('(', ')', ';'):
+            properties.update(get_property())
+        while pos < len(input) and input[pos] != ')':
+            children.append(get_node())
+        pos += 1
+        return SgfTree(properties, children)  
+        
+    if not input.startswith('('):
+        raise ValueError('tree missing')
+    return get_node()
