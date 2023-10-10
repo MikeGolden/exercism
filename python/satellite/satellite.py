13d36340ebfbdf2fc79663076141028051528f37
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 17:43:33 +0200

    satellite solution

--- python/satellite/satellite.py
+++ python/satellite/satellite.py
@@ -1,2 +1,22 @@
 def tree_from_traversals(preorder, inorder):
-    pass
+    if len(preorder) != len(set(preorder)):
+        raise ValueError("traversals must have the same length")
+    elif len(inorder) != len(set(inorder)):
+        raise ValueError("traversals must contain unique items")
+    elif set(preorder) != set(inorder):
+        raise ValueError("traversals must have the same elements")
+        
+    return build_from_traversals(preorder, inorder)
+
+
+def build_from_traversals(preorder, inorder):
+    if preorder == []:
+        return {}
+
+    i = inorder.index(preorder[0])
+
+    return {
+        "v": preorder[0],
+        "l": build_from_traversals(preorder[1:i + 1], inorder[:i]),
+        "r": build_from_traversals(preorder[i + 1:], inorder[i + 1:]),
+    }
