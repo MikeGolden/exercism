7d8aecac90a9849f65ae731f316f097cb4cf031d
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 12:27:59 +0200

    palindrome products solution

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -8,7 +8,27 @@ def largest(min_factor, max_factor):
              Iterable should contain both factors of the palindrome in an arbitrary order.
     """
 
-    pass
+    if max_factor < min_factor:
+        raise ValueError("min must be <= max")
+
+    value = None
+    factors = []
+
+    for a in range(max_factor, min_factor - 1, -1):
+        if value is not None and value > a * max_factor:
+            break
+
+        for b in range(max_factor, a - 1, -1):
+            if is_palindrome(a * b):
+                if value is None or value < a * b:
+                    value = a * b
+                    factors = [[a, b]]
+                elif value == a * b:
+                    factors.append([a, b])
+                else:
+                    break
+
+    return (value, factors)
 
 
 def smallest(min_factor, max_factor):
@@ -21,4 +41,36 @@ def smallest(min_factor, max_factor):
     Iterable should contain both factors of the palindrome in an arbitrary order.
     """
 
-    pass
+    if max_factor < min_factor:
+        raise ValueError("min must be <= max")
+
+    value = None
+    factors = []
+
+    for a in range(min_factor, max_factor + 1):
+        if value is not None and value < a * a:
+            break
+
+        for b in range(a, max_factor + 1):
+            if is_palindrome(a * b):
+                if value is None or value > a * b:
+                    value = a * b
+                    factors = [[a, b]]
+                elif value == a * b:
+                    factors.append([a, b])
+                else:
+                    break
+
+    return (value, factors)
+
+def is_palindrome(number):
+    if number % 10 == 0:
+        return False
+
+    rev = 0
+
+    while number > rev:
+        rev = rev * 10 + number % 10
+        number //= 10
+
+    return number == rev or number == rev // 10
