b3b45a384c27d86f8f58e527cbe6491b3adef7ce
Mike Goldenberg <csrutil@tuta.io>
2023-10-10 17:30:18 +0200

    two bucket right comments and types

--- python/two-bucket/two_bucket.py
+++ python/two-bucket/two_bucket.py
@@ -1,28 +1,48 @@
-def measure(bucket_one, bucket_two, goal, start_bucket):
+from typing import Union
+
+def measure(bucket_one: int, bucket_two: int, goal: int, start_bucket: str) -> Union[int, str, int]:
+    # Create a set to keep track of visited states
     visited = set()
+    # Create a queue for BFS
     queue = []
+
+    # Determine the initial state based on the starting bucket
     if start_bucket == "one":
-        queue.append((bucket_one, 0, 1))
-        invalid = (0, bucket_two)
+        initial_state = (bucket_one, 0, 1)  # Initial state: (bucket 1, bucket 2, step)
+        invalid_state = (0, bucket_two)  # Invalid state where bucket 1 is empty and bucket 2 is full
     else:
-        queue.append((0, bucket_two, 1))
-        invalid = (bucket_one, 0)
+        initial_state = (0, bucket_two, 1)  # Initial state: (bucket 1, bucket 2, step)
+        invalid_state = (bucket_one, 0)  # Invalid state where bucket 2 is empty and bucket 1 is full
+
+    queue.append(initial_state)
+
     while queue:
         b1, b2, step = queue.pop(0)
-        if b1 == goal: return step, 'one', b2
-        if b2 == goal: return step, 'two', b1
-            
-        if (b1, b2) in visited or (b1, b2) == invalid:
+
+        # Check if the goal is reached in either bucket
+        if b1 == goal:
+            return step, 'one', b2
+        if b2 == goal:
+            return step, 'two', b1
+
+        # Check if the current state is visited or invalid
+        if (b1, b2) in visited or (b1, b2) == invalid_state:
             continue
+
         visited.add((b1, b2))
-        # Pouring one bucket into the other bucket until either: a) the first bucket is empty b) the second bucket is full
+
+        # Perform possible actions:
+        # 1. Pouring one bucket into the other bucket
         queue.append((min(b1 + b2, bucket_one), b2 - (min(b1 + b2, bucket_one) - b1), step + 1))
         queue.append((b1 - (min(b1 + b2, bucket_two) - b2), min(b1 + b2, bucket_two), step + 1))
-        # Emptying a bucket and doing nothing to the other.
+
+        # 2. Emptying a bucket and doing nothing to the other
         queue.append((b1, 0, step + 1))
         queue.append((0, b2, step + 1))
-        # Filling a bucket and doing nothing to the other.
+
+        # 3. Filling a bucket and doing nothing to the other
         queue.append((bucket_one, b2, step + 1))
         queue.append((b1, bucket_two, step + 1))
+
+    # If no solution is found, raise a ValueError
     raise ValueError("No Solution Possible")
-    
