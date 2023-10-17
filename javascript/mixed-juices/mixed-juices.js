0cd1885be22e2014a1cd2ee2a6dae22e754534c6
Mike Goldenberg <csrutil@tuta.io>
2023-10-17 14:14:59 +0200

    mixed juices js solution

--- javascript/mixed-juices/mixed-juices.js
+++ javascript/mixed-juices/mixed-juices.js
@@ -11,7 +11,18 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Please implement the timeToMixJuice function');
+  switch (name) {
+    case 'Pure Strawberry Joy':
+      return 0.5;
+    case 'Energizer':
+    case 'Green Garden':
+      return 1.5;
+    case 'Tropical Island':
+      return 3;
+    case 'All or Nothing':
+      return 5;
+  }
+  return 2.5;
 }
 
 /**
@@ -23,7 +34,31 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Please implement the limesToCut function');
+  if (wedgesNeeded === 0) return 0;
+  let totalWedgesCut = 0;
+  let i = 0;
+  while(totalWedgesCut <= wedgesNeeded) {
+    if (i >= limes.length) {
+      break;
+    }
+    let wedges = 0;
+    switch(limes[i]) {
+      case "small":
+        wedges = 6;
+        break;
+      case "medium":
+        wedges = 8;
+        break;
+      case "large":
+        wedges = 10;
+        break;
+      default:
+        break;
+    }
+    totalWedgesCut += wedges;
+    i += 1;
+  }
+  return i;
 }
 
 /**
@@ -34,5 +69,15 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Please implement the remainingOrders function');
+  let time = 0;
+  while (time < timeLeft) {
+    let juice = orders.shift();
+    if (juice) {
+      let timeToPrepare = timeToMixJuice(juice);
+      time += timeToPrepare;
+    } else {
+      break;
+    }
+  }
+  return orders;
 }
