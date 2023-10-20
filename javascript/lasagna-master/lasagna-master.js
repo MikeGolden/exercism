777d269b34d38b4f0a63168a7373777d40c4a64b
Mike Goldenberg <csrutil@tuta.io>
2023-10-20 13:16:13 +0200

    lasagna master js solution

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,3 +10,34 @@
  *   ...
  * }
  */
+
+export function cookingStatus(remainingTime)  {
+  if (remainingTime === 0) return 'Lasagna is done.';
+  if (remainingTime > 0)  return 'Not done, please wait.'
+  return 'You forgot to set the timer.';
+};
+
+export function preparationTime(layers, avgTime) {
+  return layers.length * (avgTime || 2);
+}
+
+export function quantities(layers) {
+  let noodles = layers.filter((i) => i === 'noodles').length;
+  let sauce = layers.filter((i) => i === 'sauce').length;
+  return {
+    noodles: noodles * 50,
+    sauce: sauce * 0.2,
+  };
+}
+
+export function addSecretIngredient(friendsList, myList) {
+  myList.push(friendsList[friendsList.length - 1]);
+}
+
+export function scaleRecipe(recipe, numb) {
+  let ratio = numb / 2;
+  return Object.entries(recipe).reduce((a, [k, v]) => {
+    a[k] = v * ratio;
+    return a;
+  }, {});
+}
