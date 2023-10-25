2c63cc93a15ecc85af464a5ed599b0645732aa07
Mike Goldenberg <csrutil@tuta.io>
2023-10-25 12:17:02 +0200

    gigasecond js solution

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+const GIGASECOND = 1e12;
+
+export const gigasecond = (date) => new Date(date.getTime() + GIGASECOND);
