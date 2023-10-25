6e38c4b72388d10ecd4cda2ca99504a2463c6cf9
Mike Goldenberg <csrutil@tuta.io>
2023-10-25 21:39:38 +0200

    bob js solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -4,5 +4,34 @@
 //
 
 export const hey = (message) => {
-  throw new Error('Remove this statement and implement this function');
+  // Remove leading and trailing whitespace to check for empty message
+  message = message.trim();
+
+  // Check if the message is empty (no content)
+  if (message === "") {
+    return 'Fine. Be that way!';
+  }
+
+  // The message is a question (ends with a question mark)
+  const isQuestion = message.endsWith('?');
+
+  // The message is in all capitals (yelling)
+  const isShouting = message === message.toUpperCase() && message !== message.toLowerCase();
+
+  if (isQuestion && isShouting) {
+    return "Calm down, I know what I'm doing!";
+  }
+
+  if (isQuestion) {
+    return "Sure.";
+  }
+
+  // Check if the message is in all capitals (yelling)
+  if (isShouting) {
+    return 'Whoa, chill out!';
+  }
+
+  // Default response if none of the above conditions are met
+  return 'Whatever.';
 };
+
