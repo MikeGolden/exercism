ce17ad5ff12d8096ecd96289bed243dbb53530af
Mike Goldenberg <csrutil@tuta.io>
2023-10-25 15:40:47 +0200

    pangram js solution

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isPangram = (sentence) => {
+  // Create a set to keep track of encountered letters
+  const letterSet = new Set();
+
+  // Convert the sentence to lowercase for case insensitivity
+  const lowerCaseSentence = sentence.toLowerCase();
+
+  // Iterate through each character in the sentence
+  for (let char in lowerCaseSentence) {
+    // Check if the character is a letter (a to z)
+    if (/[a-z]/.test(char)) {
+      letterSet.add(char); // Add the letter to the set 
+    }
+  }
+
+  // Check if all 26 letters have been encountered
+  return letterSet.size === 26;
 };
