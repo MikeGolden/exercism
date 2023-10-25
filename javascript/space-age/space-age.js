42087a19630cdb5dc0b89183133dc1d9bef79e8c
Mike Goldenberg <csrutil@tuta.io>
2023-10-25 15:30:17 +0200

    space age js solution

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,6 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
+export const age = (planet, ageInSeconds) => {
+  // Define the orbital periods relative to Earth's orbital period (in seconds)
+  const earthYear = 31557600; // Earth's orbital period in seconds
+
+  const orbitalPeriods = {
+    'earth': earthYear, 
+    'mercury': 0.2408467 * earthYear,
+    'venus': 0.61519726 * earthYear,
+    'mars': 1.8808158 * earthYear,
+    'jupiter': 11.862615 * earthYear,
+    'saturn': 29.447498 * earthYear,
+    'uranus': 84.016846 * earthYear,
+    'neptune': 164.79132 * earthYear,
+  };
+
+  // Calculate the age on the specified planet
+  const ageOnPlanet = ageInSeconds / orbitalPeriods[planet];
+
+  // Round the age to two decimal places
+  return parseFloat(ageOnPlanet.toFixed(2));
 };
