85a02f2ed54aa9ba3e8e1eb6e00e1fb01c88d3f6
Mike Goldenberg <csrutil@tuta.io>
2023-10-26 14:07:59 +0200

    pascals triangle js solution

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -3,6 +3,20 @@
 // convenience to get you started writing code faster.
 //
 
-export const rows = () => {
-  throw new Error('Remove this statement and implement this function');
+export const rows = (numRows) => {
+  const triangle = [];
+
+  for (let i = 0; i < numRows; i++) {
+    triangle[i] = [];
+
+    for (let j = 0; j < i + 1; j++) {
+      if (j === 0 || j === i) {
+        triangle[i][j] = 1;
+      } else {
+        triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+      }
+    }
+  }
+
+  return triangle;
 };
