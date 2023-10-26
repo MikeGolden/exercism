3d01b885627ed404d2253630eca6a66bf0cd66a9
Mike Goldenberg <csrutil@tuta.io>
2023-10-26 14:09:46 +0200

    pascals triangle js comments

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -4,19 +4,22 @@
 //
 
 export const rows = (numRows) => {
-  const triangle = [];
+  const triangle = []; // Initialize an empty array to represent Pascal's Triangle
 
   for (let i = 0; i < numRows; i++) {
-    triangle[i] = [];
+    triangle[i] = []; // Initialize a sub-array for each row in the triangle
 
     for (let j = 0; j < i + 1; j++) {
+      // Iterate through each element in the current row
       if (j === 0 || j === i) {
+        // If it's the first or last element of the row, set it to 1
         triangle[i][j] = 1;
       } else {
+        // Otherwise, calculate the value based on the values from the previous row
         triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
       }
     }
   }
 
-  return triangle;
+  return triangle; // Return the complete Pascal's Triangle
 };
