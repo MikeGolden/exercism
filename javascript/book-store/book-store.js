7211a7deb21ebfdb760d410ead4262723e5340bd
Mike Goldenberg <csrutil@tuta.io>
2023-10-26 11:50:27 +0200

    book store js comments

--- javascript/book-store/book-store.js
+++ javascript/book-store/book-store.js
@@ -8,18 +8,27 @@ const DISCOUNT = [1, 0.95, 0.9, 0.8, 0.75]; // Discounts for 0 to 5 different bo
 export const cost = (books) => {
   const bookSet = [];
   
+  // Create an array 'bookSet' to store the count of unique books in each set
+
   while (books.length > 0) {
+    // Continue this loop while there are books in the 'books' array
     const set = new Set(books);
+    // Create a set to store the unique books in the current set
     bookSet.push(set.size);
+    // Add the count of unique books to 'bookSet'
 
     for (let element of set) {
+      // Iterate through the unique books in the set
       books.splice(books.indexOf(element), 1);
+      // Remove one instance of the book from the 'books' array
     }
   }
 
   while (bookSet.includes(3) && bookSet.includes(5)) {
+    // Check if there is a set of 3 and a set of 5 books
     bookSet.splice(bookSet.indexOf(3), 1);
     bookSet.splice(bookSet.indexOf(5), 1);
+    // Replace the sets of 3 and 5 books with two sets of 4 books
     bookSet.push(4, 4);
   }
 
@@ -28,4 +37,5 @@ export const cost = (books) => {
       accumulator + currentValue * BOOK_PRICE * DISCOUNT[currentValue - 1],
     0
   );
+  // Calculate the total cost by summing the cost of each set
 };
