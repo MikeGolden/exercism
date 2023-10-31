96af6c0685503f7e7c453d2c8d7967732fe32da5
Mike Goldenberg <csrutil@tuta.io>
2023-10-31 11:31:08 +0100

    robot name js solution

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,6 +1,44 @@
 // This is only a SKELETON file for the 'Robot Name' exercise. It's been
 // provided as a convenience to get your started writing code faster.
 
-export class Robot {}
+export class Robot {
+  static _namePool = new Map();
 
-Robot.releaseNames = () => {};
+  constructor() {
+    this._name = this._newName();
+  }
+
+  _randomName() {
+    const letters = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+    const numbers = [..."0123456789"];
+  
+    return (
+      letters[~~(Math.random() * 26)] +
+      letters[~~(Math.random() * 26)] +
+      numbers[~~(Math.random() * 10)] +
+      numbers[~~(Math.random() * 10)] +
+      numbers[~~(Math.random() * 10)]
+    );
+  }
+
+  _newName() {
+    let name;
+    do {
+      name = this._randomName();
+    } while (Robot._namePool.has(name));
+    Robot._namePool.set(name, true);
+    return name;
+  }
+
+  reset() {
+    this._name = this._newName();
+  }
+
+  get name() {
+    return this._name;
+  }
+}
+
+Robot.releaseNames = () => {
+  Robot._namePool = new Map();
+};
