c102362a4de8fa46cfcf8834c6136578fb2c9bf3
Mike Goldenberg <csrutil@tuta.io>
2023-10-31 12:54:38 +0100

    clock js solution

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -4,23 +4,50 @@
 //
 
 export class Clock {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(hours, minutes = 0) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.normalize();
   }
 
   toString() {
-    throw new Error('Remove this statement and implement this function');
+    return `${this.pad(this.hours)}:${this.pad(this.minutes)}`;
   }
 
-  plus() {
-    throw new Error('Remove this statement and implement this function');
+  plus(minutes) {
+    this.minutes += minutes;
+    this.normalize();
+    return this;
   }
 
-  minus() {
-    throw new Error('Remove this statement and implement this function');
+  minus(minutes) {
+    this.minutes -= minutes;
+    this.normalize();
+    return this;
   }
 
-  equals() {
-    throw new Error('Remove this statement and implement this function');
+  equals(other) {
+    return this.hours === other.hours && this.minutes === other.minutes;
   }
+
+  normalize() {
+    const hoursFromMinutes = Math.floor(this.minutes / 60);
+    this.hours += hoursFromMinutes;
+    this.minutes -= hoursFromMinutes * 60;
+    this.hours = this.hours % 24;
+    
+    if (this.hours < 0) {
+      this.hours += 24;
+    }
+
+    if (this.minutes < 0) {
+      this.minutes += 60;
+      this.hours -= 1;
+    }
+  }
+
+  pad(number) {
+    return number.toString().padStart(2, "0");
+  }
+  
 }
