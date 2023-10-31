fca32e834b032328f72f8676d045b4048e0a55f8
Mike Goldenberg <csrutil@tuta.io>
2023-10-31 11:43:50 +0100

    simple cipher js solution

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -2,21 +2,51 @@
 // This is only a SKELETON file for the 'Simple Cipher' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+const ALPHABET_LENGTH = ALPHABET.length;
+
 
 export class Cipher {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(key = null) {
+    this._key = key || this.generateKey();
   }
 
-  encode() {
-    throw new Error('Remove this statement and implement this function');
+  encode(plainText) {
+    return Array.from(plainText)
+      .map((value, index) => {
+        const n =
+          this.letterIndex(value) +
+          this.letterIndex(this._key[index % this._key.length]);
+
+        return ALPHABET.charAt(n % ALPHABET_LENGTH);
+      })
+      .join("");
   }
 
-  decode() {
-    throw new Error('Remove this statement and implement this function');
+  decode(cipherText) {
+    return Array.from(cipherText)
+      .map((value, index) => {
+        const n = 
+          this.letterIndex(value) -
+          this.letterIndex(this.key[index % this._key.length]);
+        return ALPHABET.charAt((n + ALPHABET_LENGTH) % ALPHABET_LENGTH);
+      })
+      .join("");
   }
 
   get key() {
-    throw new Error('Remove this statement and implement this function');
+    return this._key;
+  }
+
+  generateKey(length = 10) {
+    return [...Array(length)].map((_) => this.randomLetter()).join("");
+  }
+
+  randomLetter() {
+    return ALPHABET.charAt(Math.floor(Math.random() * ALPHABET_LENGTH));
+  }
+
+  letterIndex(letter) {
+    return ALPHABET.indexOf(letter);
   }
 }
