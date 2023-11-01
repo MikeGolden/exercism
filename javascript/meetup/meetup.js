402ddb9594d3b964c4703372f6028db6717f3a8b
Mike Goldenberg <csrutil@tuta.io>
2023-11-01 13:12:06 +0100

    meetup js solution 2

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -1,40 +1,20 @@
-export const meetup = (year, month, dayOfWeek, descriptor) => {
-  // Map of descriptors to the corresponding range of days
-  const descriptorMap = {
-    'first': 1,
-    'second': 8,
-    'third': 15,
-    'fourth': 22,
-    'fifth': 29,
-    'last': 35,
-    'montheenth': 13,
-    'tuesteenth': 19,
-    'wednesteenth': 25,
-    'thursteenth': 31,
-    'friteenth': 26,
-    'saturteenth': 23,
-    'sunteenth': 30,
-  };
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+]
 
-  // Map of days of the week to the corresponding numbers
-  const dayOfWeekMap = {
-    'monday': 1,
-    'tuesday': 2,
-    'wednesday': 3,
-    'thursday': 4,
-    'friday': 5,
-    'saturday': 6,
-    'sunday': 0,
-  };
+const firstDates = { first: 1, second: 8, third: 15, fourth: 22, teenth: 13 } 
 
-  // Create a new Date object for the first day of the specified month and year
-  let date = new Date(year, month - 1, descriptorMap[descriptor]);
+export const meetup = (year, month, descriptor, weekday) => {
+  const dayIndex = weekdays.indexOf(weekday)
+  const firstDateInRange = firstDates[descriptor] || new Date(year, (month), 0).getDate() - 6
+  const firstDay = new Date(year, month-1, firstDateInRange).getDay()
+  const date = firstDateInRange + (dayIndex - firstDay + 7) % 7
 
-  // Use a loop to find the correct day of the week
-  while (date.getDay() !== dayOfWeekMap[dayOfWeek]) {
-    date.setDate(date.getDate() + 1);
-  }
-
-  // Return the date
-  return date;
-};
+  return new Date(year, month-1, date)
+}
