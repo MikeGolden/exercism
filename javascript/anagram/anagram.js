d209e5700578115c2b026ef436400a8b50c9a08a
Mike Goldenberg <csrutil@tuta.io>
2023-11-01 09:47:40 +0100

    anagram js solution

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -3,6 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
-export const findAnagrams = () => {
-  throw new Error('Remove this statement and implement this function');
+export const findAnagrams = (word, list) => {
+  const wordSorted = word.toLowerCase().split("").sort().join("");
+  const anagrams = [];
+  
+  for (let i = 0; i < list.length; i++) {
+    const sortedWord = list[i].toLowerCase().split("").sort().join("");
+    if (
+      wordSorted === sortedWord &&
+      word.toLowerCase() !== list.toLowerCase()
+    ) {
+      anagrams.push(list[i]);
+    }
+  }
+  return anagrams;
 };
