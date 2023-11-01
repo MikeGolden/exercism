67cc78c393e3abd03153c2480c896308eb05aa51
Mike Goldenberg <csrutil@tuta.io>
2023-11-01 09:48:51 +0100

    anagram js comments

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,20 +1,25 @@
-//
-// This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// The `findAnagrams` function takes a `word` and a `list` of words and returns an array of anagrams of the `word` from the `list`.
 
 export const findAnagrams = (word, list) => {
+  // Convert the input `word` to lowercase, split it into an array of characters, sort the characters, and then join them back into a string.
   const wordSorted = word.toLowerCase().split("").sort().join("");
   const anagrams = [];
-  
+
+  // Iterate through the `list` of words.
   for (let i = 0; i < list.length; i++) {
+    // For each word in the list, convert it to lowercase, split it into an array of characters, sort the characters, and then join them back into a string.
     const sortedWord = list[i].toLowerCase().split("").sort().join("");
+
+    // Compare the sorted word with the sorted input `word` to check if they are anagrams.
+    // Also, ensure that the compared words are not the same by checking their lowercase versions.
     if (
       wordSorted === sortedWord &&
-      word.toLowerCase() !== list.toLowerCase()
+      word.toLowerCase() !== list[i].toLowerCase()
     ) {
+      // If they are anagrams and not the same word, add the word to the `anagrams` array.
       anagrams.push(list[i]);
     }
   }
+  // Return the array of anagrams.
   return anagrams;
 };
