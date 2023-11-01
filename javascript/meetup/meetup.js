e1bb8baca5d404c5bc3f492d691322d97bf5f1ea
Mike Goldenberg <csrutil@tuta.io>
2023-11-01 16:31:09 +0100

    meetup js comments

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -1,20 +1,31 @@
+// Define an array of weekdays
 const weekdays = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday'
+ 'Sunday',
+ 'Monday',
+ 'Tuesday',
+ 'Wednesday',
+ 'Thursday',
+ 'Friday',
+ 'Saturday'
 ]
 
+// Define an object that maps descriptors to the corresponding range of days
 const firstDates = { first: 1, second: 8, third: 15, fourth: 22, teenth: 13 } 
 
+// Define the meetup function
 export const meetup = (year, month, descriptor, weekday) => {
-  const dayIndex = weekdays.indexOf(weekday)
-  const firstDateInRange = firstDates[descriptor] || new Date(year, (month), 0).getDate() - 6
-  const firstDay = new Date(year, month-1, firstDateInRange).getDay()
-  const date = firstDateInRange + (dayIndex - firstDay + 7) % 7
+ // Get the index of the weekday in the weekdays array
+ const dayIndex = weekdays.indexOf(weekday)
 
-  return new Date(year, month-1, date)
+ // Get the first date in the range based on the descriptor
+ const firstDateInRange = firstDates[descriptor] || new Date(year, (month), 0).getDate() - 6
+
+ // Get the day of the week of the first date in the range
+ const firstDay = new Date(year, month-1, firstDateInRange).getDay()
+
+ // Calculate the date of the meetup
+ const date = firstDateInRange + (dayIndex - firstDay + 7) % 7
+
+ // Return the date of the meetup
+ return new Date(year, month-1, date)
 }
