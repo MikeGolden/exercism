205112248eda69b4aaff6af5a1b9bd42c382823f
Mike Goldenberg <csrutil@tuta.io>
2023-11-02 11:46:58 +0100

    high scores js solution 2

--- javascript/high-scores/high-scores.js
+++ javascript/high-scores/high-scores.js
@@ -1,21 +1,21 @@
 export class HighScores {
   constructor(scores) {
-    this.scores = scores;
+    this._scores = scores;
   }
 
   get scores() {
-    return this.scores;
+    return this._scores;
   }
 
   get latest() {
-    return this.scores[this.scores.length - 1];
+    return this._scores[this._scores.length - 1];
   }
 
   get personalBest() {
-    return Math.max(...this.scores);
+    return Math.max(...this._scores);
   }
 
   get personalTopThree() {
-    return this.scores.sort((a, b) => b - a).slice(0, 3);
+    return this._scores.sort((a, b) => b - a).slice(0, 3);
   }
 }
