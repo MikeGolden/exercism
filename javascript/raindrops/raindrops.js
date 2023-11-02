c32b0a5cd6a5410c94f1e57d7ea32b935f56dea1
Mike Goldenberg <csrutil@tuta.io>
2023-11-02 11:32:00 +0100

    raindrops js solution

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -1,8 +1,23 @@
-//
-// This is only a SKELETON file for the 'Raindrops' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const convert = (number) => {
+  // Initialize an empty string to store the raindrop sounds
+  let sounds = '';
 
-export const convert = () => {
-  throw new Error('Remove this statement and implement this function');
+  // Check if the number has 3, 5 or 7 as a factor
+  if (number % 3 === 0) {
+    sounds += 'Pling';
+  }
+  if (number % 5 === 0) {
+    sounds += 'Plang';
+  }
+  if (number % 7 === 0) {
+    sounds += 'Plong';
+  }
+
+  // If the number does not have any of 3, 5 or 7 as a factor,
+  // convert the number to a string and add it to the string
+  if (sounds === '') {
+    sounds = number.toString();
+  }
+
+  return sounds;
 };
