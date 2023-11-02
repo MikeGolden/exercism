93a164be4a6ba6b0b7d4f0fc31ff43916a1d96e4
Mike Goldenberg <csrutil@tuta.io>
2023-11-02 11:44:30 +0100

    high scores js solution

--- javascript/high-scores/high-scores.js
+++ javascript/high-scores/high-scores.js
@@ -1,26 +1,21 @@
-//
-// This is only a SKELETON file for the 'High Scores' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class HighScores {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(scores) {
+    this.scores = scores;
   }
 
   get scores() {
-    throw new Error('Remove this statement and implement this function');
+    return this.scores;
   }
 
   get latest() {
-    throw new Error('Remove this statement and implement this function');
+    return this.scores[this.scores.length - 1];
   }
 
   get personalBest() {
-    throw new Error('Remove this statement and implement this function');
+    return Math.max(...this.scores);
   }
 
   get personalTopThree() {
-    throw new Error('Remove this statement and implement this function');
+    return this.scores.sort((a, b) => b - a).slice(0, 3);
   }
 }
