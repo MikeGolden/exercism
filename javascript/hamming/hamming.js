f67956440bc3f58d8f620f9d299c046d3156021a
Mike Goldenberg <csrutil@tuta.io>
2023-11-02 11:20:34 +0100

    hamming js solution

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,8 +1,20 @@
-//
-// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const compute = (dna1, dna2) => {
+  // Check if the two DNA strands are equal length
+  if (dna1.length !== dna2.length) {
+    throw new Error("DNA strands must be of equal length.");
+  }
 
-export const compute = () => {
-  throw new Error('Remove this statement and implement this function');
+  // Initialize a counter for the Hamming Distance
+  let distance = 0;
+
+  // Traverse the two DNA strands simnultaneously
+  for (let i = 0; i < dna1.length; i++) {
+    // Compare each corresponding character in the two DNA strands
+    if (dna1[i] !== dna2[i]) {
+      // If the characters are not the same, increment the counter
+      distance++;
+    }
+  }
+
+  return distance;
 };
