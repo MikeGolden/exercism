44ef3548eb81b6c25fcd2e035c19d6990caf7542
Mike Goldenberg <csrutil@tuta.io>
2023-11-02 14:06:53 +0100

    scale generator js solution

--- javascript/scale-generator/scale-generator.js
+++ javascript/scale-generator/scale-generator.js
@@ -1,18 +1,35 @@
-//
-// This is only a SKELETON file for the 'Scale Generator' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Scale {
   constructor(tonic) {
-    throw new Error('Remove this statement and implement this function');
+    // Convert the tonic to uppercase 
+    this.tonic = tonic.toUpperCase();
+
+    // Generate the chromatic scale 
+    this.chromaticScale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
   }
 
   chromatic() {
-    throw new Error('Remove this statement and implement this function');
+    // Return the chromatic scale
+    return this.chromaticScale;
   }
 
   interval(intervals) {
-    throw new Error('Remove this statement and implement this function');
+    // Determine the index of the tonic in the chromatic scale
+    let index = this.chromaticScale.indexOf(this.tonic);
+
+    // Generate the scale
+    const scale = intervals.split('').map(interval => {
+      switch (interval) {
+        case 'M':
+          index += 2;
+          break;
+        case 'm':
+          index += 1;
+          break;
+      }
+      // Return the note at the new index
+      return this.chromaticScale[index % 12];
+    });
+
+    return scale;
   }
 }
