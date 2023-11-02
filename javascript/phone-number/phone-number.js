a76fe98cd69a9dc26f19a1b8f75b34e054058db3
Mike Goldenberg <csrutil@tuta.io>
2023-11-02 13:17:51 +0100

    phone number js solution

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,8 +1,7 @@
-//
-// This is only a SKELETON file for the 'Phone Number' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const clean = () => {
-  throw new Error('Remove this statement and implement this function');
+export const clean = (phoneNumber) => {
+  let cleanPhoneNumber = phoneNumber.replace(/\D/g, '');
+  if (cleanPhoneNumber.startsWith('1')) {
+    cleanPhoneNumber = cleanPhoneNumber.slice(1);
+  }
+  return cleanPhoneNumber;
 };
