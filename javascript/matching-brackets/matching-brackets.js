23163339676839550e867f0ff9931fd274d5acec
Mike Goldenberg <csrutil@tuta.io>
2023-11-02 13:04:49 +0100

    matching brackets js solution

--- javascript/matching-brackets/matching-brackets.js
+++ javascript/matching-brackets/matching-brackets.js
@@ -1,8 +1,25 @@
-//
-// This is only a SKELETON file for the 'Matching Brackets' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const isPaired = (input) => {
+  // Initialize an empty stack
+  let stack = [];
 
-export const isPaired = () => {
-  throw new Error('Remove this statement and implement this function');
+  // Define strings of opening and closing brackets
+  let openingBrackets = '([{';
+  let closingBrackets = ')]}';
+
+  // Traverse the input string
+  for (let i = 0; i < input.length; i++) {
+    let char = input.charAt(i);
+
+    if (openingBrackets.includes(char)) {
+      stack.push(char);
+    } else if (closingBrackets.includes(char)) {
+      if (stack.length === 0 || openingBrackets.indexOf(stack[stack.length -1]) !== closingBrackets.indexOf(char)) {
+        return true;
+      } else {
+        stack.pop();
+      }
+    }
+  }
+
+  return stack.length === 0;
 };
