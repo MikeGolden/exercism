379e18ef3c665a99638a5462105a6e4d8e880425
Mike Goldenberg <csrutil@tuta.io>
2023-11-06 09:03:58 +0100

    series js solution

--- javascript/series/series.js
+++ javascript/series/series.js
@@ -1,14 +1,14 @@
-//
-// This is only a SKELETON file for the 'Series' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Series {
   constructor(series) {
-    throw new Error('Remove this statement and implement this function');
+    this.series = series;
   }
 
   slices(sliceLength) {
-    throw new Error('Remove this statement and implement this function');
+    const slices = [];
+    for (let i = 0; i <= this.series.length - sliceLength; i++) {
+      slices.push(this.series.substring(i, i + sliceLength));
+    }
+
+    return slices;
   }
 }
