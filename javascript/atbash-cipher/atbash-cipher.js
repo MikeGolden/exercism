151f1f0d56fac9bc9a8efd97eb97edd3899a01b3
Mike Goldenberg <csrutil@tuta.io>
2023-11-09 13:03:14 +0100

    atbash cipher js solution

--- javascript/atbash-cipher/atbash-cipher.js
+++ javascript/atbash-cipher/atbash-cipher.js
@@ -1,12 +1,23 @@
-//
-// This is only a SKELETON file for the 'Atbash Cipher' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const encode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const encode = (string) => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetReverse = alphabet.split("").reverse().join("");
+  const encoded = string
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .replace(/[a-z]/g, (c) => alphabetReverse[alphabet.indexOf(c)]);
+    
+  return encoded
+    .split("")
+    .reduce((acc, c, i) => (i % 5 === 0 ? acc + " " + c : acc + c), "")
+    .trim();
 };
 
-export const decode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decode = (string) => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetReverse = alphabet.split("").reverse().join("");
+
+  return string
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .replace(/[a-z]/g, (c) => alphabetReverse[alphabet.indexOf(c)]);
 };
