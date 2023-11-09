07566812da7670833baeb39d24e4c866eed0499d
Mike Goldenberg <csrutil@tuta.io>
2023-11-09 12:26:59 +0100

    nucleotide count js solution

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,8 +1,21 @@
-//
-// This is only a SKELETON file for the 'Nucleotide Count' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export function countNucleotides(strand) {
-  throw new Error('Remove this statement and implement this function');
+  // Create a counting object that maps each nucleotide to its count 
+  const counts = {'A': 0, 'C': 0, 'G': 0, 'T': 0};
+
+  // Traverse the input string 
+  for (let i = 0; i < strand.length; i++) {
+    // Get current character
+    let char = strand.charAt(i);
+
+    // If the character is a nucleotide, increment its count in the count
+    if (counts.hasOwnProperty(char)) {
+      counts[char]++;
+    }
+    // If the character is not a nucleotide, throw an error
+    else {
+      throw new Error('Invalid DNA sequence');
+    }
+  }
+
+  return `${counts['A']} ${counts['C']} ${counts['G']} ${counts['T']}`;
 }
