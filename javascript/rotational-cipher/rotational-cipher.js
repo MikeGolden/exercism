de59045d17c384d11905a2646a258b811579376c
Mike Goldenberg <csrutil@tuta.io>
2023-11-13 11:40:53 +0100

    rotational cipher js solution

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,12 +1,8 @@
-export const rotate = (text, key) => {
-  // Convert the text to uppercase
-  const upperText = text.toLowerCase();
-
-  // Replace each letter in the text with a letter that is shifted by the key in the alphabet
-  const result = upperText.replace(/[A-Z]/g, (c) =>
-    String.fromCharCode(((c.charCodeAt(0) - 65 + key) % 26) + 65),
-  );
-
-  // Return the result
-  return result;
-};
+export function rotate(message, r) {
+    const a = 'a'.charCodeAt(0),
+        A = 'A'.charCodeAt(0)
+    return [...message].map(c =>
+            /[A-Z]/.test(c) ? String.fromCharCode(A + (c.charCodeAt(0) - A + r) % 26) :
+            /[a-z]/.test(c) ? String.fromCharCode(a + (c.charCodeAt(0) - a + r) % 26) : c)
+        .join('')
+}
