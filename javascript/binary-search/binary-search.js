cd74fef672a57d235176de3e0a7186ace95412c9
Mike Goldenberg <csrutil@tuta.io>
2023-11-13 15:11:08 +0100

    binary search js solution

--- javascript/binary-search/binary-search.js
+++ javascript/binary-search/binary-search.js
@@ -1,8 +1,19 @@
-//
-// This is only a SKELETON file for the 'Binary Search' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const find = (sortedArray, target) => {
+  let low = 0;
+  let high = sortedArray.length - 1;
 
-export const find = () => {
-  throw new Error('Remove this statement and implement this function');
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const midValue = sortedArray[mid];
+
+    if (midValue === target) {
+      return mid;
+    } else if (midValue < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  throw new Error("Value not in array");
 };
