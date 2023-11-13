e1b27870d136ada103810f5b1d1a51d1cc45e109
Mike Goldenberg <csrutil@tuta.io>
2023-11-13 12:48:02 +0100

    simple linked list js solution 3

--- javascript/simple-linked-list/simple-linked-list.js
+++ javascript/simple-linked-list/simple-linked-list.js
@@ -1,62 +1,64 @@
+//
+// This is only a SKELETON file for the 'Simple Linked List' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
 export class Element {
-  constructor(value) {
-    this._value = value;
-    this._next = null;
-  }
-
-  get value() {
-    return this._value;
-  }
-
-  get next() {
-    return this._next;
-  }
-
-  set next(nextElement) {
-    this._next = nextElement;
-  }
-}
-
-export class List {
-  constructor() {
-    this.head = null;
-    this.length = 0;
-  }
-
-  add(nextValue) {
-    const newElement = new Element(nextValue, this.head);
-    this.head = newElement;
-    this.length++;
-  }
-
-  get length() {
-    return this.length;
-  }
-
-  get head() {
-    return this.head;
-  }
-
-  toArray() {
-    let elements = [];
-    let current = this.head;
-    while (current) {
-      elements.push(current.value);
-      current = current.next;
+    constructor(value) {
+      this._value = value;
+      this._next = null;
     }
-    return elements;
-  }
-
-  reverse() {
-    let previous = null;
-    let current = this.head;
-    let next = null;
-    while (current) {
-      next = current.next;
-      current.next = previous;
-      previous = current;
-      current = next;
+  
+    get value() {
+      return this._value
     }
-    this.head = previous;
+  
+    get next() {
+      return this._next;
+    }
+
+    set next(nextElement){
+      this._next = nextElement;
+    }
   }
-}
+  
+  export class List {
+    constructor(array) {
+      this._list = []
+
+      if(array){
+        array.forEach((e) => {this.add(new Element(e))});
+      }
+
+    }
+  
+    add(nextValue) {
+      this._list.push(nextValue)
+      if (this.length > 1 ) this.head.next = this._list[this.length-2];
+    }
+  
+    get length() {
+      return this._list.length
+    }
+  
+    get head() {
+        let retHead = this._list[this.length-1];
+        return retHead == undefined ? null : retHead;
+    }
+  
+    toArray() {
+      let retArray = [];
+      let element = this.head
+      for (let i = this.length; i >0; i--) {
+        retArray.push(element.value)
+        element = element.next;
+      }
+
+      return retArray
+    }
+  
+    reverse() {
+      return new List(this.toArray())
+    }
+  }
+  
