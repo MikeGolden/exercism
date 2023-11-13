e1b314b2596e1752eea14d91b12c25c6c0520c14
Mike Goldenberg <csrutil@tuta.io>
2023-11-13 11:50:44 +0100

    roman numerals js solution

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,8 +1,33 @@
-//
-// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const toRoman = (num) => {
+  // Create an array of Roman numerals in descending order
+  const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const syb = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
 
-export const toRoman = () => {
-  throw new Error('Remove this statement and implement this function');
+  // Traverse the array
+  let roman_num = "";
+  for (let i = 0; i < val.length; i++) {
+    // For each numeral, while the number is greater than or equal the value of the numeral
+    while (num >= val[i]) {
+      // Substract the value of the numeral from the number
+      num -= val[i];
+      // Append the numeral to the result
+      roman_num += syb[i];
+    }
+  }
+
+  return roman_num;
 };
