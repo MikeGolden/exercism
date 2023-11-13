5241b9a62af272cafe80d7171d693d336a9cd2db
Mike Goldenberg <csrutil@tuta.io>
2023-11-13 11:20:29 +0100

    transpose js solution

--- javascript/transpose/transpose.js
+++ javascript/transpose/transpose.js
@@ -1,8 +1,22 @@
-//
-// This is only a SKELETON file for the 'Transpose' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const transpose = (textstring) => {
+  // Split the text into lines
+  const lines = textstring.split("\n");
 
-export const transpose = () => {
-  throw new Error('Remove this statement and implement this function');
+  // Find the length of the longest line
+  const maxLength = lines.reduce((max, line) => Math.max(max, line.length), 0);
+
+  // Pad all lines with spaces to make them the same length
+  lines.forEach((line, i) => (lines[i] = line.padEnd(maxLength, " ")));
+
+  // Transpose the text by swapping characters in each column
+  let transposedText = "";
+  for (let i = 0; i < maxLength; i++) {
+    for (let line of lines) {
+      transposedText += line[i];
+    }
+    transposedText += "\n";
+  }
+
+  // Format the transposed text as a string and return it
+  return transposedText.trim();
 };
