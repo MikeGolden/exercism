e3785933e72be5651c06524e1a5f8a17da78e3ec
Mike Goldenberg <csrutil@tuta.io>
2023-11-14 15:01:20 +0100

    promises js solution

--- javascript/promises/promises.js
+++ javascript/promises/promises.js
@@ -1,24 +1,83 @@
-//
-// This is only a SKELETON file for the 'Promises' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const promisify = () => {
-  throw new Error('Remove this statement and implement this function');
+export const promisify = (originalFunction) => {
+  return (...args) => {
+    return new Promise((resolve, reject) => {
+      originalFunction(...args, (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  };
 };
 
-export const all = () => {
-  throw new Error('Remove this statement and implement this function');
+export const all = (promises) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let completed = 0;
+
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((result) => {
+          results[index] = result;
+          completed++;
+
+          if (completed === promises.length) {
+            resolve(results);
+          }
+        })
+        .catch(reject);
+    });
+  });
 };
 
-export const allSettled = () => {
-  throw new Error('Remove this statement and implement this function');
+export const allSettled = (promises) => {
+  return new Promise((resolve) => {
+    const results = [];
+    let completed = 0;
+
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((result) => {
+          results[index] = { status: "fulfilled", value: result };
+        })
+        .catch((reason) => {
+          results[index] = { status: "rejected", reason };
+        })
+        .finally(() => {
+          completed++;
+
+          if (completed === promises.length) {
+            resolve(results);
+          }
+        });
+    });
+  });
 };
 
-export const race = () => {
-  throw new Error('Remove this statement and implement this function');
+export const race = (promises) => {
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      Promise.resolve(promise).then(resolve, reject);
+    });
+  });
 };
 
-export const any = () => {
-  throw new Error('Remove this statement and implement this function');
+export const any = (promises) => {
+  return new Promise((resolve, reject) => {
+    const errors = [];
+
+    promises.forEach((promise) => {
+      Promise.resolve(promise)
+        .then(resolve)
+        .catch((error) => {
+          error.push(error);
+
+          if (errors.length === promises.length) {
+            reject(errors);
+          }
+        });
+    });
+  });
 };
