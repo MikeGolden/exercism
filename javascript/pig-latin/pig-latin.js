a3c4240893d0f8fb61716c1c46960bbddb5e7a61
Mike Goldenberg <csrutil@tuta.io>
2023-11-14 13:09:52 +0100

    pig latin js solution

--- javascript/pig-latin/pig-latin.js
+++ javascript/pig-latin/pig-latin.js
@@ -1,8 +1,35 @@
-//
-// This is only a SKELETON file for the 'Pig Latin' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const translate = (english) => {
+  const words = english.split(" ");
+  const vowels = ["a", "e", "i", "o", "u"];
 
-export const translate = () => {
-  throw new Error('Remove this statement and implement this function');
+  const isVowel = (char) => vowels.includes(char.toLowerCase());
+  const isConsonantCluster = (word) => {
+    for (let i = 0; i < word.length; i++) {
+      if (isVowel(word[i])) {
+        return i > 0;
+      }
+    }
+    return false;
+  };
+
+  const translateWord = (word) => {
+    if (isVowel(word[0]) || word.startsWith("xr") || word.startsWith("yt")) {
+      return word + "ay";
+    } else if (isConsonantCluster(word) || word.startsWith("qu")) {
+      let consonantCluster = "";
+      let i = 0;
+      while (
+        i < word.length &&
+        (isConsonantCluster(word.substring(i)) || word.startsWith("qu", i))
+      ) {
+        consonantCluster += word[i];
+        i++;
+      }
+      return word.substring(i) + consonantCluster + "ay";
+    } else {
+      return word.substring(1) + word[0] + "ay";
+    }
+  };
+
+  return words.map(translateWord).join(" ");
 };
