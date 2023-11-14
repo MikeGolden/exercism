c7e4c2c65cec38471cfd8adb34e3a718fce1ea3c
Mike Goldenberg <csrutil@tuta.io>
2023-11-14 21:24:05 +0100

    spiral matrix js solution

--- javascript/spiral-matrix/spiral-matrix.js
+++ javascript/spiral-matrix/spiral-matrix.js
@@ -1,8 +1,37 @@
-//
-// This is only a SKELETON file for the 'Spiral Matrix' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const spiralMatrix = (size) => {
+  if (size <= 0) {
+    return [];
+  }
 
-export const spiralMatrix = () => {
-  throw new Error('Remove this statement and implement this function');
+  const matrix = Array.from({ length: size }, () => Array(size).fill(0));
+
+  let num = 1;
+  let top = 0;
+  let bottom = size - 1;
+  let left = 0;
+  let right = size - 1;
+
+  while (top <= bottom && left <= right) {
+    for (let i = left; i <= right; i++) {
+      matrix[top][i] = num++;
+    }
+    top++;
+
+    for (let i = top; i <= bottom; i++) {
+      matrix[i][right] = num++;
+    }
+    right--;
+
+    for (let i = right; i >= left; i--) {
+      matrix[bottom][i] = num++;
+    }
+    bottom--;
+
+    for (let i = bottom; i >= top; i--) {
+      matrix[i][left] = num++;
+    }
+    left++;
+  }
+
+  return matrix;
 };
