e15c0eee5aa3fb689dbd5de900b190b399d12853
Mike Goldenberg <csrutil@tuta.io>
2023-11-15 13:40:16 +0100

    protein translation js solution

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -1,8 +1,42 @@
-//
-// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const translate = (rna) => {
+  if (!rna) {
+    return [];
+  }
 
-export const translate = () => {
-  throw new Error('Remove this statement and implement this function');
+  const codonMap = {
+    AUG: "Methionine",
+    UUU: "Phenylalanine",
+    UUC: "Phenylalanine",
+    UUA: "Leucine",
+    UUG: "Leucine",
+    UCU: "Serine",
+    UCC: "Serine",
+    UCA: "Serine",
+    UCG: "Serine",
+    UAU: "Tyrosine",
+    UAC: "Tyrosine",
+    UGU: "Cysteine",
+    UGC: "Cysteine",
+    UGG: "Tryptophan",
+    UAA: "STOP",
+    UAG: "STOP",
+    UGA: "STOP",
+  };
+
+  const codons = rna.match(/.{1,3}/g) || [];
+
+  const proteins = [];
+  for (const codon of codons) {
+    if (!codonMap[codon]) {
+      throw new Error("Invalid codon");
+    }
+
+    if (codonMap[codon] === "STOP") {
+      break;
+    }
+
+    proteins.push(codonMap[codon]);
+  }
+
+  return proteins;
 };
