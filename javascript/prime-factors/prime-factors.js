00f3902b18caeb7b54b4122dc285dbb919a61141
Mike Goldenberg <csrutil@tuta.io>
2023-11-15 13:31:16 +0100

    prime factors js solution

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,8 +1,18 @@
-//
-// This is only a SKELETON file for the 'Prime Factors' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const primeFactors = (number) => {
+  if (number <= 1) {
+    return [];
+  }
 
-export const primeFactors = () => {
-  throw new Error('Remove this statement and implement this function');
+  const factors = [];
+  let divisor = 2;
+
+  while (number > 1) {
+    while (number % divisor === 0) {
+      factors.push(divisor);
+      number /= divisor;
+    }
+    divisor++;
+  }
+
+  return factors;
 };
