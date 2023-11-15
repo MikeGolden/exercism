5ba97b524abafab9f5db1273842dfef5e0ab82b7
Mike Goldenberg <csrutil@tuta.io>
2023-11-15 13:56:46 +0100

    rail fence cipher js solution

--- javascript/rail-fence-cipher/rail-fence-cipher.js
+++ javascript/rail-fence-cipher/rail-fence-cipher.js
@@ -1,12 +1,66 @@
-//
-// This is only a SKELETON file for the 'Rail Fence Cipher' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const encode = (text, rails) => {
+  if (rails <= 1) {
+    return text;
+  }
 
-export const encode = () => {
-  throw new Error('Remove this statement and implement this function');
+  const fence = Array.from({ length: rails }, () =>
+    Array(text.length).fill("."),
+  );
+  let row = 0;
+  let direction = 1;
+
+  for (let i = 0; i < text.length; i++) {
+    fence[row][i] = text[i];
+    row += direction;
+
+    if (row === 0 || row === rails - 1) {
+      direction *= -1;
+    }
+  }
+
+  return fence.map((row) => row.join("")).join("");
 };
 
-export const decode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decode = (text, rails) => {
+  if (rails <= 1) {
+    return text;
+  }
+
+  const fence = Array.from({ length: rails }, () =>
+    Array(text.length).fill("."),
+  );
+  let row = 0;
+  let direction = 1;
+
+  for (let i = 0; i < text.length; i++) {
+    fence[row][i] = "X";
+    row += direction;
+
+    if (row === 0 || row === rails - 1) {
+      direction *= -1;
+    }
+  }
+
+  let index = 0;
+  for (let i = 0; i < rails; i++) {
+    for (let j = 0; j < text.length; j++) {
+      if (fence[i][j] === "X") {
+        fence[i][j] = text[index++];
+      }
+    }
+  }
+
+  row = 0;
+  direction = 1;
+  let result = "";
+
+  for (let i = 0; i < text.length; i++) {
+    result += fence[row][i];
+    row += direction;
+    if (row === 0 || row === rails - 1) {
+      direction *= -1;
+    }
+  }
+
+  return result;
 };
