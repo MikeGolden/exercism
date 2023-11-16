0a8add53a261f1ce6f4dd08110ef571ffa6e779e
Mike Goldenberg <csrutil@tuta.io>
2023-11-16 13:17:46 +0100

    palindrome products js solution 2

--- javascript/palindrome-products/palindrome-products.js
+++ javascript/palindrome-products/palindrome-products.js
@@ -1,35 +1,59 @@
+//
+// This is only a SKELETON file for the 'Palindrome Products' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+
+const isPalindrome = n => String(n) === String(n).split('').reverse().join('');
+const EMPTY_RESULT = {value: null, factors: []}
 export class Palindromes {
-  static isPalindrome(num) {
-    const str = num.toString();
-    return str === str.split("").reverse().join("");
+  static generate(options) {
+    return new Palindromes(options);
   }
-
-  static generate(min, max) {
-    let smallest = Infinity;
-    let largest = -Infinity;
-    const products = {};
-
-    for (let i = min; i <= max; i++) {
-      for (let j = i; j <= max; j++) {
-        const product = i * j;
-        if (this.isPalindrome(product)) {
-          if (product < smallest) {
-            smallest = product;
-            product[smallest] = [[i, j]];
-          } else if (product === smallest) {
-            products[smallest].push([i, j]);
-          }
-
-          if (product > largest) {
-            largest = product;
-            products[largest] = [[i, j]];
-          } else if (product === largest) {
-            products[largest].push([i, j]);
-          }
-        }
+  constructor({minFactor, maxFactor}) {
+    this.min = minFactor ?? 1;
+    this.max = maxFactor;
+  }
+  checkLimits() {
+    if (this.min > this.max)
+      throw new Error('min must be <= max');
+  }
+  get smallest() {
+    this.checkLimits();
+    const limit = this.max ** 2;
+    for (let p = this.min ** 2; p <= limit; p++) {
+      const result = this.isPalindromeProduct(p);
+      if (result) return result;
+    }
+    return EMPTY_RESULT;
+  }
+  get largest() {
+    this.checkLimits();
+    const limit = this.min ** 2;
+    for (let p = this.max ** 2; p >= limit; p--) {
+      const result = this.isPalindromeProduct(p);
+      if (result) return result
+    }
+    return EMPTY_RESULT;
+  }
+  isPalindromeProduct(n) {
+    if (isPalindrome(n)) {
+      const fs = this.factors(n);
+      if (fs.length > 0)
+        return {value: n, factors: fs};
+    }
+    return;
+  }
+  factors(n) {
+    const pairs = [];
+    const limit = Math.min(this.max, Math.sqrt(n));
+    for (let i = this.min; i <= limit; i++) {
+      if (n % i === 0) {
+        const j = n / i;
+        if (this.min <= j && j <= this.max)
+          pairs.push([i, j]);
       }
     }
-
-    return { smallest, largest, factorPairs: products };
+    return pairs;
   }
 }
