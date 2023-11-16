14115b322c824e302c3e77320e19e586d1996a3d
Mike Goldenberg <csrutil@tuta.io>
2023-11-16 12:30:14 +0100

    twelve days js solution

--- javascript/twelve-days/twelve-days.js
+++ javascript/twelve-days/twelve-days.js
@@ -1,8 +1,48 @@
-//
-// This is only a SKELETON file for the 'Twelve Days' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const recite = () => {
-  throw new Error('Remove this statement and implement this function');
+  const gifts = [
+    "a partridge in a pear tree",
+    "two turtle doves",
+    "three French hens",
+    "four calling birds",
+    "five golden rings",
+    "six geese a-laying",
+    "seven swans a-swimming",
+    "eight maids a-milking",
+    "nine ladies dancing",
+    "ten lords a-leaping",
+    "eleven pipers piping",
+    "twelve drummers drumming",
+  ];
+
+  const days = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth",
+  ];
+
+  let lyrics = "";
+  for (let i = 0; i < 12; i++) {
+    lyrics += `On the ${days[i]} day of Christmas, my true love gave to me\n`;
+    for (let j = i; j >= 0; j--) {
+      if (j === 0 && i !== 0) {
+        lyrics += `And ${gifts[j]}\n`;
+      } else {
+        lyrics += `${gifts[j]}\n`;
+      }
+    }
+    if (i !== 11) {
+      lyrics += "\n";
+    }
+  }
+
+  return lyrics;
 };
