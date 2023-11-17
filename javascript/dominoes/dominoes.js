68f58da3345c3a8404a88d4249679b0218bbce42
Mike Goldenberg <csrutil@tuta.io>
2023-11-17 12:40:45 +0100

    dominoes js comments

--- javascript/dominoes/dominoes.js
+++ javascript/dominoes/dominoes.js
@@ -1,63 +1,69 @@
-//
-// This is only a SKELETON file for the 'Dominoes' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
+// Main function to create a domino chain
 export const chain = (dominoes) => {
   if (dominoes.length === 0) {
     return [];
   }
   
-  const head = dominoes[dominoes.length-1];
+  // Get the last domino as the starting point for the chain
+  const head = dominoes[dominoes.length - 1];
   return helperChain([head], dominoes.slice(0, -1));
 };
+
+// Recursive function to build the domino chain
 const helperChain = (currentChain, dominoesLeft) => {
+  // Get the head and tail values of the current chain
   const head = currentChain[0][0];
   const tail = currentChain[currentChain.length - 1][1];
+  
+  // Check if there are no dominoes left to add to the chain
   if (dominoesLeft.length === 0) {
     return head === tail ? currentChain : null;
   }
   
+  // Iterate through the remaining dominoes
   for (let i = 0; i < dominoesLeft.length; i++) {
     const first = dominoesLeft[i][0];
     const second = dominoesLeft[i][1];
     const newDomLeft = [...dominoesLeft];
     newDomLeft.splice(i, 1);
     
+    // Check if the current head matches the first or second value of the domino
     if (head === first) {
-      const possibleChain = helperChain (
-        [[second, first], ...currentChain], newDomLeft
+      const possibleChain = helperChain(
+        [[second, first], ...currentChain],
+        newDomLeft
       );
       if (possibleChain !== null) {
         return possibleChain;
       }
-    }
-    else if (head === second) {
-      const possibleChain = helperChain (
-        [[first, second], ...currentChain], newDomLeft
+    } else if (head === second) {
+      const possibleChain = helperChain(
+        [[first, second], ...currentChain],
+        newDomLeft
       );
       if (possibleChain !== null) {
         return possibleChain;
       }
     }
     
+    // Check if the current tail matches the first or second value of the domino
     if (tail === first) {
-      const possibleChain = helperChain (
-        [...currentChain, [first, second]], newDomLeft
+      const possibleChain = helperChain(
+        [...currentChain, [first, second]],
+        newDomLeft
+      );
+      if (possibleChain !== null) {
+        return possibleChain;
+      }
+    } else if (tail === second) {
+      const possibleChain = helperChain(
+        [...currentChain, [second, first]],
+        newDomLeft
       );
       if (possibleChain !== null) {
         return possibleChain;
       }
     } 
-    else if (tail === second) {
-      const possibleChain = helperChain (
-        [...currentChain, [second, first]], newDomLeft
-      );
-      if (possibleChain !== null) {
-        return possibleChain;
-      }
-    } 
-    
   }
   return null;
 };
