15c2fc0ac8f3af7ea65a7a9b419c09a8dcc92556
Mike Goldenberg <csrutil@tuta.io>
2023-11-17 12:38:30 +0100

    dominoes js solution

--- javascript/dominoes/dominoes.js
+++ javascript/dominoes/dominoes.js
@@ -1,8 +1,30 @@
-//
-// This is only a SKELETON file for the 'Dominoes' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const chain = (dominoes) => {
+  const buildChain = (chaining) => {
+    if (chaining.length === dominoes.length) {
+      if (chaining[0][0] === chaining[chain.length - 1][1]) {
+        return chaining;
+      }
+      return null;
+    }
 
-export const chain = () => {
-  throw new Error('Remove this statement and implement this function');
+    for (let i = 0; i < dominoes.length; i++) {
+      const [a, b] = dominoes[i];
+      if (a === chaining[chaining.length - 1][1]) {
+        const result = buildChain([...chaining, [a, b]]);
+        if (result) {
+          return result;
+        }
+      }
+    }
+
+    return null;
+  };
+
+  for (let i = 0; i < dominoes.length; i++) {
+    const result = buildChain([dominoes[i]]);
+    if (result) {
+      return result;
+    }
+  }
+  return null;
 };
