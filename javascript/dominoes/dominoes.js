e5aef110e57c8c2025c68534a1ccb8b05562246a
Mike Goldenberg <csrutil@tuta.io>
2023-11-17 12:39:46 +0100

    dominoes js solution 2

--- javascript/dominoes/dominoes.js
+++ javascript/dominoes/dominoes.js
@@ -1,30 +1,63 @@
+//
+// This is only a SKELETON file for the 'Dominoes' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
 export const chain = (dominoes) => {
-  const buildChain = (chaining) => {
-    if (chaining.length === dominoes.length) {
-      if (chaining[0][0] === chaining[chain.length - 1][1]) {
-        return chaining;
+  if (dominoes.length === 0) {
+    return [];
+  }
+  
+  const head = dominoes[dominoes.length-1];
+  return helperChain([head], dominoes.slice(0, -1));
+};
+const helperChain = (currentChain, dominoesLeft) => {
+  const head = currentChain[0][0];
+  const tail = currentChain[currentChain.length - 1][1];
+  if (dominoesLeft.length === 0) {
+    return head === tail ? currentChain : null;
+  }
+  
+  for (let i = 0; i < dominoesLeft.length; i++) {
+    const first = dominoesLeft[i][0];
+    const second = dominoesLeft[i][1];
+    const newDomLeft = [...dominoesLeft];
+    newDomLeft.splice(i, 1);
+    
+    if (head === first) {
+      const possibleChain = helperChain (
+        [[second, first], ...currentChain], newDomLeft
+      );
+      if (possibleChain !== null) {
+        return possibleChain;
       }
-      return null;
     }
-
-    for (let i = 0; i < dominoes.length; i++) {
-      const [a, b] = dominoes[i];
-      if (a === chaining[chaining.length - 1][1]) {
-        const result = buildChain([...chaining, [a, b]]);
-        if (result) {
-          return result;
-        }
+    else if (head === second) {
+      const possibleChain = helperChain (
+        [[first, second], ...currentChain], newDomLeft
+      );
+      if (possibleChain !== null) {
+        return possibleChain;
       }
     }
-
-    return null;
-  };
-
-  for (let i = 0; i < dominoes.length; i++) {
-    const result = buildChain([dominoes[i]]);
-    if (result) {
-      return result;
-    }
+    
+    if (tail === first) {
+      const possibleChain = helperChain (
+        [...currentChain, [first, second]], newDomLeft
+      );
+      if (possibleChain !== null) {
+        return possibleChain;
+      }
+    } 
+    else if (tail === second) {
+      const possibleChain = helperChain (
+        [...currentChain, [second, first]], newDomLeft
+      );
+      if (possibleChain !== null) {
+        return possibleChain;
+      }
+    } 
+    
   }
   return null;
 };
