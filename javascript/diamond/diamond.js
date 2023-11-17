4f7f74364a8ba7a097fc026b0d5fb6d5ce56a058
Mike Goldenberg <csrutil@tuta.io>
2023-11-17 12:58:27 +0100

    diamond js comments

--- javascript/diamond/diamond.js
+++ javascript/diamond/diamond.js
@@ -1,11 +1,25 @@
 export const rows = (text) => {
-  const limit = text.charCodeAt(0) - 65
-  let spaces = ' '.repeat(limit)
-  const result = [spaces + 'A' + spaces]
+  // Calculate the distance from 'A' based on the input letter
+  const limit = text.charCodeAt(0) - 65;
+
+  // Create spaces to center the 'A' at the first row
+  let spaces = ' '.repeat(limit);
+
+  // Initialize the result array with the first row containing 'A'
+  const result = [spaces + 'A' + spaces];
+
+  // Loop to construct the upper half of the diamond
   for (let i = 1; i <= limit; i++) {
-    spaces = ' '.repeat(limit - i)
-    const char = String.fromCharCode(i + 65)
-    result.push(spaces + char + ' '.repeat(2 * i - 1) + char + spaces)
+    // Calculate spaces to center the characters in each row
+    spaces = ' '.repeat(limit - i);
+
+    // Get the character for the current row
+    const char = String.fromCharCode(i + 65);
+
+    // Construct each row in the upper half of the diamond
+    result.push(spaces + char + ' '.repeat(2 * i - 1) + char + spaces);
   }
-  return result.concat(result.slice(0, -1).reverse())
+
+  // Combine the upper half with the reversed upper half (excluding the last row)
+  return result.concat(result.slice(0, -1).reverse());
 }
