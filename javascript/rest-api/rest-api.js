092e0af354c5fb6ba16469379ef4bbaece8b2f5b
Mike Goldenberg <csrutil@tuta.io>
2023-11-22 20:50:50 +0100

    rest api js comments

--- javascript/rest-api/rest-api.js
+++ javascript/rest-api/rest-api.js
@@ -1,58 +1,62 @@
-//
-// This is only a SKELETON file for the 'Rest API' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class RestAPI {
   constructor(db) {
-    this.db = db
+    this.db = db;
   }
 
   get(url) {
+    // Handling GET requests
     switch (url) {
-      case '/users': return this.db
-      default: 
-        let { groups: { name } } = /(=)(?<name>\w+)/.exec(url)
-        let matching = this.db.users.filter((x) => x.name === name)
-        return {users: matching}
-        
+      case '/users':
+        // Return the entire user database
+        return this.db;
+      default:
+        // Extract the user name from the URL
+        let { groups: { name } } = /(=)(?<name>\w+)/.exec(url);
+        // Find the user with the matching name
+        let matching = this.db.users.filter((x) => x.name === name);
+        return { users: matching };
     }
   }
 
   post(url, payload) {
+    // Handling POST requests
     switch (url) {
-      case '/add': 
-        let user = {name: payload.user, owes: {}, owed_by: {}, balance: 0,}
-        this.db.users.push(user)
-        return user
+      case '/add':
+        // Adding a new user to the database
+        let user = { name: payload.user, owes: {}, owed_by: {}, balance: 0 };
+        this.db.users.push(user);
+        return user;
       case '/iou':
+        // Finding the lender and borrower from the payload
+        let lender, borrower;
         for (const user of this.db.users) {
           if (user.name === payload.lender) {
-            var lender = user
+            lender = user;
           } else if (user.name === payload.borrower) {
-            var borrower = user
+            borrower = user;
           } else {
-            this.db.users.splice(this.db.users.indexOf(user), 1)
+            this.db.users.splice(this.db.users.indexOf(user), 1);
           }
         }
+        // Handling the IOU transaction
         if (lender.owes[borrower.name] > payload.amount) {
-          borrower.owed_by[lender.name] = (borrower.owed_by[lender.name] || 0) - payload.amount
-          lender.owes[borrower.name] = (lender.owes[borrower.name] || 0) - payload.amount
-        } else  { 
-          lender.owed_by[borrower.name] = payload.amount - (lender.owes[borrower.name] || 0)
-          delete lender.owes[borrower.name]
-          borrower.owes[lender.name] = payload.amount - (borrower.owed_by[lender.name] || 0)
-          delete borrower.owed_by[lender.name]
+          borrower.owed_by[lender.name] = (borrower.owed_by[lender.name] || 0) - payload.amount;
+          lender.owes[borrower.name] = (lender.owes[borrower.name] || 0) - payload.amount;
+        } else {
+          lender.owed_by[borrower.name] = payload.amount - (lender.owes[borrower.name] || 0);
+          delete lender.owes[borrower.name];
+          borrower.owes[lender.name] = payload.amount - (borrower.owed_by[lender.name] || 0);
+          delete borrower.owed_by[lender.name];
         }
-        if (lender.owed_by[borrower.name] === 0 ) {
-          delete lender.owed_by[borrower.name]
+        if (lender.owed_by[borrower.name] === 0) {
+          delete lender.owed_by[borrower.name];
         }
-        if ( borrower.owes[lender.name] === 0) {
-          delete borrower.owes[lender.name]
+        if (borrower.owes[lender.name] === 0) {
+          delete borrower.owes[lender.name];
         }
-        lender.balance += payload.amount
-        borrower.balance -= payload.amount
-        return this.db
+        lender.balance += payload.amount;
+        borrower.balance -= payload.amount;
+        return this.db;
     }
   }
 }
