dc8e803b314147a53b9eb40137d621dc0b0d5777
Mike Goldenberg <csrutil@tuta.io>
2023-11-22 20:17:12 +0100

    pythagorean triplet js solution

--- javascript/pythagorean-triplet/pythagorean-triplet.js
+++ javascript/pythagorean-triplet/pythagorean-triplet.js
@@ -1,18 +1,29 @@
-//
-// This is only a SKELETON file for the 'Pythagorean Triplet' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export function triplets({ minFactor, maxFactor, sum }) {
-  throw new Error('Remove this statement and implement this function');
+  const tripletsArr = [];
+
+  for (let a = minFactor; a < sum / 3; a++) {
+    for (let b = a + 1; b < sum / 2; b++) {
+      const c = sum - a - b;
+      if (
+        a * a + b * b === c * c &&
+        (!maxFactor || (a <= maxFactor && b <= maxFactor && c <= maxFactor))
+      ) {
+        tripletsArr.push(new Triplet(a, b, c));
+      }
+    }
+  }
+
+  return tripletsArr;
 }
 
 class Triplet {
   constructor(a, b, c) {
-    throw new Error('Remove this statement and implement this function');
+    this.a = a;
+    this.b = b;
+    this.c = c;
   }
 
   toArray() {
-    throw new Error('Remove this statement and implement this function');
+    return [this.a, this.b, this.c];
   }
 }
