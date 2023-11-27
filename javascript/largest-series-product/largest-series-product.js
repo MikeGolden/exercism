acd4530e95ac0537c8a2d1588b57e248dc2b75a2
Mike Goldenberg <csrutil@tuta.io>
2023-11-27 16:01:43 +0100

    largest series product js solution

--- javascript/largest-series-product/largest-series-product.js
+++ javascript/largest-series-product/largest-series-product.js
@@ -1,8 +1,23 @@
-//
-// This is only a SKELETON file for the 'Largest Series Product' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const largestProduct = (input, span) => {
+  if (span < 0 || !/^\d+$/.test(input) || span > input.length) {
+    throw new Error("Invalid input or span");
+  }
 
-export const largestProduct = () => {
-  throw new Error('Remove this statement and implement this function');
+  if (span === 0) {
+    return 1;
+  }
+
+  let maxProduct = 0;
+
+  for (let i = 0; i <= input.length - span; i++) {
+    let product = 1;
+    for (let j = i; j < i + span; j++) {
+      product *= Number(input[j]);
+    }
+    if (product > maxProduct) {
+      maxProduct = product;
+    }
+  }
+
+  return maxProduct;
 };
