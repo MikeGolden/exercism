dcc3d46e9ab27f4e07fdfe8b353eca1b2e357be0
Mike Goldenberg <csrutil@tuta.io>
2023-11-27 16:18:44 +0100

    rational numbers js solution

--- javascript/rational-numbers/rational-numbers.js
+++ javascript/rational-numbers/rational-numbers.js
@@ -1,42 +1,58 @@
-//
-// This is only a SKELETON file for the 'Rational Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// Helper function to calculate the greatest common divisor (gcd)
+const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
 
 export class Rational {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(numerator, denominator) {
+    const divisor = gcd(Math.abs(numerator), Math.abs(denominator));
+    this.numerator = numerator / divisor;
+    this.denominator = denominator / divisor;
+
+    // Ensure that the denominator is positive
+    if (this.denominator < 0) {
+      this.numerator *= -1;
+      this.denominator *= -1;
+    }
   }
 
-  add() {
-    throw new Error('Remove this statement and implement this function');
+  add(other) {
+    const newNumerator =
+      this.numerator * other.denominator + other.numerator * this.denominator;
+    const newDenominator = this.denominator * other.denominator;
+    return new Rational(newNumerator, newDenominator);
   }
 
-  sub() {
-    throw new Error('Remove this statement and implement this function');
+  sub(other) {
+    const newNumerator =
+      this.numerator * other.denominator - other.numerator * this.denominator;
+    const newDenominator = this.denominator * other.denominator;
+    return new Rational(newNumerator, newDenominator);
   }
 
-  mul() {
-    throw new Error('Remove this statement and implement this function');
+  mul(other) {
+    const newNumerator = this.numerator * other.numerator;
+    const newDenominator = this.denominator * other.denominator;
+    return new Rational(newNumerator, newDenominator);
   }
 
-  div() {
-    throw new Error('Remove this statement and implement this function');
+  div(other) {
+    const newNumerator = this.numerator * other.denominator;
+    const newDenominator = this.denominator * other.numerator;
+    return new Rational(newNumerator, newDenominator);
   }
 
   abs() {
-    throw new Error('Remove this statement and implement this function');
+    return new Rational(Math.abs(this.numerator), Math.abs(this.denominator));
   }
 
-  exprational() {
-    throw new Error('Remove this statement and implement this function');
+  exprational(power) {
+    return new Rational(this.numerator ** power, this.denominator ** power);
   }
 
-  expreal() {
-    throw new Error('Remove this statement and implement this function');
+  expreal(exponent) {
+    return Math.pow(Math.pow(this.numerator, exponent), 1 / this.denominator);
   }
 
   reduce() {
-    throw new Error('Remove this statement and implement this function');
+    return this;
   }
 }
