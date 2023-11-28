fa238999993f2c5a39c21cd0500cf6852b698826
Mike Goldenberg <csrutil@tuta.io>
2023-11-28 13:33:59 +0100

    go counting js solution 2

--- javascript/go-counting/go-counting.js
+++ javascript/go-counting/go-counting.js
@@ -1,64 +1,61 @@
+const PLAYERS = new Map([
+  ["B", "BLACK"],
+  ["W", "WHITE"],
+  ["", "NONE"],
+]);
+
+const pCoord = (p) => p.split(",").map(Number);
+
 export class GoCounting {
   constructor(board) {
-    this.board = board;
-    this.visited = Array(board.length)
-      .fill(0)
-      .map(() => Array(board[0].length).fill(false));
-    this.territories = { BLACK: [], WHITE: [], NONE: [] };
+    this.board = new Map();
+    for (let j = 0; j < board.length; j++) {
+      let line = board[j].split("");
+      for (let i = 0; i < line.length; i++)
+        this.board.set(`${i},${j}`, line[i]);
+    }
   }
 
-  isValid(x, y) {
-    return (
-      x >= 0 && x < this.board.length && y >= 0 && y < this.board[0].length
-    );
-  }
-
-  floodFill(x, y, player, territory) {
-    if (!this.isValid(x, y) || this.visited[x][y]) return;
-
-    if (this.board[x][y] === " ") {
-      this.territories[player].push([x, y]);
-      territory.push([x, y]);
-    } else if (this.board[x][y] !== player) {
-      return;
-    }
-
-    this.visited[x][y] = true;
-
-    const dx = [0, 0, -1, 1];
-    const dy = [-1, 1, 0, 0];
-
-    for (let i = 0; i < 4; i++) {
-      const newX = x + dx[i];
-      const newY = y + dy[i];
-      this.floodFill(newX, newY, player, territory);
-    }
+  expand(x, y, visited = new Set()) {
+    let pos = `${x},${y}`;
+    if (visited.has(pos)) return visited;
+    if (!this.board.has(pos)) return visited;
+    let val = this.board.get(pos);
+    if (/[BW]/.test(val)) return new Set([val, ...visited]);
+    visited.add(pos);
+    return [
+      [x - 1, y],
+      [x, y + 1],
+      [x, y - 1],
+      [x + 1, y],
+    ].reduce((v, nxt) => this.expand(...nxt, v), visited);
   }
 
   getTerritory(x, y) {
-    if (this.visited[x][y]) return { owner: "NONE", territory: [] };
-
-    const player = this.board[x][y];
-    const territory = [];
-    this.floodFill(x, y, player, territory);
-
-    return { owner: player, territory };
+    if (!this.board.has(`${x},${y}`)) return { error: "Invalid coordinate" };
+    let owners = [],
+      coords = [];
+    for (let v of this.expand(x, y).values())
+      (/[BW]/.test(v) ? owners : coords).push(v);
+    let owner = owners.length == 1 && coords.length > 0 ? owners[0] : "";
+    return { owner: PLAYERS.get(owner), territory: coords.sort().map(pCoord) };
   }
 
   getTerritories() {
-    for (let i = 0; i < this.board.length; i++) {
-      for (let j = 0; j < this.board[i].length; j++) {
-        if (!this.visited[i][j] && this.board[i][j] === " ") {
-          const territory = [];
-          this.floodFill(i, j, "BLACK", territory);
-          this.floodFill(i, j, "WHITE", territory);
-        }
-      }
+    let data = { BLACK: [], WHITE: [], NONE: [] };
+    Array.from(this.board.keys()).forEach((pos) => {
+      let positions = Object.values(data).flat();
+      if (positions.includes(pos)) return;
+      let t = this.getTerritory(...pCoord(pos));
+      let p = t.territory.map((p) => p.join(","));
+      data[t.owner] = (data[t.owner] ?? []).concat(p);
+    });
+    let ret = {};
+    for (let [owner, coords] of Object.entries(data)) {
+      let key = "territory" + owner.charAt(0) + owner.slice(1).toLowerCase();
+      ret[key] = coords.sort().map(pCoord);
     }
-    return {
-      territoryBlack: this.territories.BLACK,
-      territoryWhite: this.territories.WHITE,
-      territoryNone: this.territories.NONE,
-    };
+    return ret;
   }
 }
+
