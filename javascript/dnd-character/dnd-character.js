dba159e08c8185d1fa67682dada9af75a987cb73
Mike Goldenberg <csrutil@tuta.io>
2023-11-28 12:43:30 +0100

    dnd character js solution

--- javascript/dnd-character/dnd-character.js
+++ javascript/dnd-character/dnd-character.js
@@ -1,42 +1,45 @@
-//
-// This is only a SKELETON file for the 'D&D Character' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const abilityModifier = () => {
-  throw new Error('Remove this statement and implement this function');
+export const abilityModifier = (constitution) => {
+  return Math.floor((constitution - 10) / 2); // Calculate and return the ability modifier for constitution
 };
 
 export class Character {
   static rollAbility() {
-    throw new Error('Remove this statement and implement this function');
+    const rolls = Array.from(
+      { length: 4 },
+      () => Math.floor(Math.random() * 6) + 1,
+    ); // Generate 4 random dice rolls
+    rolls.sort((a, b) => b - a); // Sort the rolls in descending order
+    return rolls.slice(0, 3).reduce((acc, curr) => acc + curr, 0); // Return the sum of the three highest rolls
   }
 
+  // Getters for each ability using rollAbility() method
   get strength() {
-    throw new Error('Remove this statement and implement this function');
+    return Character.rollAbility();
   }
 
   get dexterity() {
-    throw new Error('Remove this statement and implement this function');
+    return Character.rollAbility();
   }
 
   get constitution() {
-    throw new Error('Remove this statement and implement this function');
+    return Character.rollAbility();
   }
 
   get intelligence() {
-    throw new Error('Remove this statement and implement this function');
+    return Character.rollAbility();
   }
 
   get wisdom() {
-    throw new Error('Remove this statement and implement this function');
+    return Character.rollAbility();
   }
 
   get charisma() {
-    throw new Error('Remove this statement and implement this function');
+    return Character.rollAbility();
   }
 
+  // Getter for hitpoints based on constitution modifier
   get hitpoints() {
-    throw new Error('Remove this statement and implement this function');
+    const conModifier = abilityModifier(this.constitution); // Get constitution modifier
+    return 10 + conModifier;
   }
 }
