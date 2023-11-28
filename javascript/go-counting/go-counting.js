1b5f41cb7e2323f0e7cd79b3cf5c35c7d1855e62
Mike Goldenberg <csrutil@tuta.io>
2023-11-28 13:30:12 +0100

    go counting js solution

--- javascript/go-counting/go-counting.js
+++ javascript/go-counting/go-counting.js
@@ -1,18 +1,64 @@
-//
-// This is only a SKELETON file for the 'Go Counting' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class GoCounting {
   constructor(board) {
-    throw new Error('Remove this statement and implement this function');
+    this.board = board;
+    this.visited = Array(board.length)
+      .fill(0)
+      .map(() => Array(board[0].length).fill(false));
+    this.territories = { BLACK: [], WHITE: [], NONE: [] };
+  }
+
+  isValid(x, y) {
+    return (
+      x >= 0 && x < this.board.length && y >= 0 && y < this.board[0].length
+    );
+  }
+
+  floodFill(x, y, player, territory) {
+    if (!this.isValid(x, y) || this.visited[x][y]) return;
+
+    if (this.board[x][y] === " ") {
+      this.territories[player].push([x, y]);
+      territory.push([x, y]);
+    } else if (this.board[x][y] !== player) {
+      return;
+    }
+
+    this.visited[x][y] = true;
+
+    const dx = [0, 0, -1, 1];
+    const dy = [-1, 1, 0, 0];
+
+    for (let i = 0; i < 4; i++) {
+      const newX = x + dx[i];
+      const newY = y + dy[i];
+      this.floodFill(newX, newY, player, territory);
+    }
   }
 
   getTerritory(x, y) {
-    throw new Error('Remove this statement and implement this function');
+    if (this.visited[x][y]) return { owner: "NONE", territory: [] };
+
+    const player = this.board[x][y];
+    const territory = [];
+    this.floodFill(x, y, player, territory);
+
+    return { owner: player, territory };
   }
 
   getTerritories() {
-    throw new Error('Remove this statement and implement this function');
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
+        if (!this.visited[i][j] && this.board[i][j] === " ") {
+          const territory = [];
+          this.floodFill(i, j, "BLACK", territory);
+          this.floodFill(i, j, "WHITE", territory);
+        }
+      }
+    }
+    return {
+      territoryBlack: this.territories.BLACK,
+      territoryWhite: this.territories.WHITE,
+      territoryNone: this.territories.NONE,
+    };
   }
 }
