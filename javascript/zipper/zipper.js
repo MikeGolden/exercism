054e93361b2898c4be55c61301a2be47dd9204a2
Mike Goldenberg <csrutil@tuta.io>
2023-11-29 13:31:33 +0100

    zipper js solution 2

--- javascript/zipper/zipper.js
+++ javascript/zipper/zipper.js
@@ -1,55 +1,85 @@
 export class Zipper {
-  constructor(tree, focus = null, parent = null) {
-    this.tree = tree;
-    this.focus = focus || tree;
-    this.parent = parent;
+  constructor(tree, root) {
+    [this.tree, this.root] = Zipper._clone(tree, root);
+  }
+
+  static _clone(tree, root) {
+    const new_root = Zipper._cloneTree(root);
+    const new_tree = Zipper._bindTree(tree, root, new_root);
+    return [new_tree, new_root];
+  }
+
+  static _cloneTree(tree) {
+    if (!tree) return null;
+    return {
+      value: tree.value,
+      left: Zipper._cloneTree(tree.left),
+      right: Zipper._cloneTree(tree.right),
+    };
+  }
+
+  static _bindTree(tree, root, new_root) {
+    if (root === null) return null;
+    if (tree === root) return new_root;
+
+    let binded = Zipper._bindTree(tree, root.left, new_root.left);
+    if (binded) return binded;
+    binded = Zipper._bindTree(tree, root.right, new_root.right);
+    if (binded) return binded;
+    return null;
   }
 
   static fromTree(tree) {
-    return new Zipper(tree);
+    return new Zipper(tree, tree);
   }
 
   toTree() {
-    return this.tree;
+    return this.root;
   }
 
   value() {
-    return this.focus.value;
+    return this.tree.value;
   }
 
   left() {
-    if (!this.focus.left) return null;
-
-    return new Zipper(this.tree, this.focus.left, this.focus);
+    return this.tree.left ? new Zipper(this.tree.left, this.root) : null;
   }
 
   right() {
-    if (!this.focus.right) return null;
+    return this.tree.right ? new Zipper(this.tree.right, this.root) : null;
+  }
 
-    return new Zipper(this.tree, this.focus.right, this.focus);
+  up_search(current, target) {
+    if (!current) return null;
+    if (current.left === target || current.right === target) return current;
+    let found = this.up_search(current.left, target);
+    if (found) return found;
+    found = this.up_search(current.right, target);
+    if (found) return found;
+    return null;
   }
 
   up() {
-    if (!this.parent) return null;
-
-    return new Zipper(this.tree, this.parent, this.parent.parent);
+    const parent = this.up_search(this.root, this.tree);
+    return parent ? new Zipper(parent, this.root) : null;
   }
 
-  setValue(newValue) {
-    const newTree = { ...this.tree };
-    this.focus.value = newValue;
-    return new Zipper(newTree, this.focus, this.parent);
+  setValue(value) {
+    let new_zipper = new Zipper(this.tree, this.root);
+    new_zipper.tree.value = value;
+    return new_zipper;
   }
 
-  setLeft(leftNode) {
-    const newTree = { ...this.tree };
-    this.focus.left = leftNode;
-    return new Zipper(newTree, this.focus, this.parent);
+  setLeft(tree) {
+    let new_zipper = new Zipper(this.tree, this.root);
+    new_zipper.tree.left = tree;
+    return new_zipper;
   }
 
-  setRight(rightNode) {
-    const newTree = { ...this.tree };
-    this.focus.right = rightNode;
-    return new Zipper(newTree, this.focus, this.parent);
+  setRight(tree) {
+    let new_zipper = new Zipper(this.tree, this.root);
+    new_zipper.tree.right = tree;
+    return new_zipper;
   }
 }
+
