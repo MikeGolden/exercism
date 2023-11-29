6df5f3e1ded790c886fc1e7f5c78d8276453112e
Mike Goldenberg <csrutil@tuta.io>
2023-11-29 17:48:25 +0100

    queen attack js solution 2

--- javascript/queen-attack/queen-attack.js
+++ javascript/queen-attack/queen-attack.js
@@ -1,32 +1,46 @@
 export class QueenAttack {
   constructor({
-    black: [blackRow = 7, blackColumn = 3] = [],
-    white: [whiteRow = 0, whiteColumn = 3] = [],
+    black: [blackRow, blackColumn] = [0, 3],
+    white: [whiteRow, whiteColumn] = [7, 3],
   } = {}) {
-    this.black = [blackRow, blackColumn];
+    if (
+      Math.min(whiteRow, whiteColumn, blackRow, blackColumn) < 0 ||
+      Math.max(whiteRow, whiteColumn, blackRow, blackColumn) > 7
+    )
+      throw new Error("Queen must be placed on the board");
+    if (blackRow === whiteRow && blackColumn === whiteColumn)
+      throw new Error("Queens cannot share the same space");
     this.white = [whiteRow, whiteColumn];
+    this.black = [blackRow, blackColumn];
   }
 
   toString() {
-    const board = Array.from({ length: 8 }, () => Array(8).fill("_"));
-    const [whiteRow, whiteColumn] = this.white;
-    const [blackRow, blackColumn] = this.black;
-
-    board[whiteRow][whiteColumn] = "W";
-    board[blackRow][blackColumn] = "B";
-
-    return board.map((row) => row.join(" ")).join("\n");
+    const board = [];
+    for (let i = 0; i < 8; i++) {
+      board[i] = Array.from("_ _ _ _ _ _ _ _");
+    }
+    board[this.white[0]][this.white[1] * 2] = "W";
+    board[this.black[0]][this.black[1] * 2] = "B";
+    for (let i = 0; i < 8; i++) {
+      board[i] = board[i].join("");
+    }
+    return board.join("\n");
+    //throw new Error('Remove this statement and implement this function');
   }
 
   get canAttack() {
-    const [whiteRow, whiteColumn] = this.white;
-    const [blackRow, blackColumn] = this.black;
+    //throw new Error('Remove this statement and implement this function');
+    if (this.black[0] === this.white[0] || this.black[1] == this.white[1])
+      return true;
+    if (this.isDiagonal()) return true;
+    return false;
+  }
 
-    const sameRow = whiteRow === blackRow;
-    const sameColumn = whiteColumn === blackColumn;
-    const sameDiagonal =
-      Math.abs(whiteRow - blackRow) === Math.abs(whiteColumn - blackColumn);
-
-    return sameRow || sameColumn || sameDiagonal;
+  isDiagonal() {
+    let [x, y] = this.white;
+    let [a, b] = this.black;
+    if (Math.abs(x - a) === Math.abs(y - b)) return true;
+    return false;
   }
 }
+
