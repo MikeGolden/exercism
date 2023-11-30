b5bf00d3a7eedbd252bc5b3d60ae56a8129428d5
Mike Goldenberg <csrutil@tuta.io>
2023-11-30 13:12:30 +0100

    rectangles js solution

--- javascript/rectangles/rectangles.js
+++ javascript/rectangles/rectangles.js
@@ -1,8 +1,39 @@
-//
-// This is only a SKELETON file for the 'Rectangles' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const CORNER = "+";
+const HORIZONTAL = "-";
+const VERTICAL = "|";
+const MIDDLE = "s";
+const VALID_HORIZONTALS = [CORNER, HORIZONTAL];
 
-export function count() {
-  throw new Error('Remove this statement and implement this function');
-}
+const validCrossOfLength = (length) => (crossSection) =>
+  new RegExp(
+    `[\\${CORNER}\\${VERTICAL}][\\${VERTICAL}\\${MIDDLE}\\${CORNER}\\${HORIZONTAL}]{${length}}[\\${CORNER}\\${VERTICAL}]`,
+  ).test(crossSection);
+
+const validRectangleOfLength = (length) => (crossSection) =>
+  new RegExp(
+    `\\${CORNER}[\\${CORNER}\\${HORIZONTAL}]{${length}}\\${CORNER}`,
+  ).test(crossSection);
+
+export const count = (plane = []) =>
+  plane.reduce(
+    (rectangles, [...cross], crossInd) =>
+      rectangles +
+      cross.reduce((count, char, charInd) => {
+        if (char === CORNER) {
+          for (const [i, nextChar] of [...cross.entries()].slice(charInd + 1)) {
+            if (!VALID_HORIZONTALS.includes(nextChar)) break;
+            if (nextChar === CORNER) {
+              for (const nextCross of plane.slice(crossInd + 1)) {
+                const possibleCross = nextCross.slice(charInd, i + 1);
+                if (!validCrossOfLength(i - charInd - 1)(possibleCross)) break;
+                if (validRectangleOfLength(i - charInd - 1)(possibleCross))
+                  count++;
+              }
+            }
+          }
+        }
+
+        return count;
+      }, 0),
+    0,
+  );
