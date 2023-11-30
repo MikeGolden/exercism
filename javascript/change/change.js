a4ddd19938c08f84d2c5f0344e090123f66d0ea7
Mike Goldenberg <csrutil@tuta.io>
2023-11-30 12:04:08 +0100

    change js comments

--- javascript/change/change.js
+++ javascript/change/change.js
@@ -1,34 +1,41 @@
 export class Change {
   calculate(coinArray, target) {
+    // If target is 0, no change is needed
     if (target === 0) {
-      return []; // No change needed
+      return [];
     }
 
+    // Negative totals are not allowed
     if (target < 0) {
       throw new Error("Negative totals are not allowed.");
     }
 
+    // Ensure the target can be represented with the given coins
     if (target < Math.min(...coinArray)) {
       throw new Error(
         "The total " + target + " cannot be represented in the given currency.",
       );
     }
 
+    // Initialize an array to store minimum coins needed for each target value
     const dp = new Array(target + 1).fill(Infinity);
-    dp[0] = 0;
+    dp[0] = 0; // Zero coins needed for target value of 0
 
+    // Compute minimum coins needed for each target value up to 'target'
     for (const coin of coinArray) {
       for (let i = coin; i <= target; i++) {
         dp[i] = Math.min(dp[i], dp[i - coin] + 1);
       }
     }
 
+    // If no valid solution exists for the target, throw an error
     if (dp[target] === Infinity) {
       throw new Error(
         "The total " + target + " cannot be represented in the given currency.",
       );
     }
 
+    // Find the coins used for the minimum number of coins
     const coinsUsed = [];
     let remaining = target;
 
@@ -45,4 +52,3 @@ export class Change {
     return coinsUsed;
   }
 }
-
