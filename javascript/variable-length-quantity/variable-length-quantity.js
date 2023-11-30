889e762c9e1c5722fb7cf1d55eb397db9ad72832
Mike Goldenberg <csrutil@tuta.io>
2023-11-30 14:42:31 +0100

    variable length quantity js solution

--- javascript/variable-length-quantity/variable-length-quantity.js
+++ javascript/variable-length-quantity/variable-length-quantity.js
@@ -1,12 +1,32 @@
-//
-// This is only a SKELETON file for the 'Variable Length Quantity' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const encode = (value) => {
+  let result = [];
 
-export const encode = () => {
-  throw new Error('Remove this statement and implement this function');
+  while (value > 0) {
+    let byte = value & 0x7f;
+
+    if (value > 0x7f) byte |= 0x80;
+    result.push(byte);
+    value >>= 7;
+  }
+
+  if (result.length === 0) result.push(0);
+  return result.reverse();
 };
 
-export const decode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decode = (bytes) => {
+  let result = 0;
+  let shift = 0;
+
+  for (let i = 0; i < bytes.length; i++) {
+    let byte = bytes[i] & 0x7f;
+
+    result |= byte << shift;
+    shift += 7;
+
+    if ((bytes[i] & 0x80) === 0) {
+      return result;
+    }
+  }
+
+  throw new Error("Invalid variable length quantity");
 };
