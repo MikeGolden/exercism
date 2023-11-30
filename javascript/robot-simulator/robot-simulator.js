489fc9df5047cc87329768033d5fb66bf4f8b267
Mike Goldenberg <csrutil@tuta.io>
2023-11-30 13:49:58 +0100

    robot simulator js comments

--- javascript/robot-simulator/robot-simulator.js
+++ javascript/robot-simulator/robot-simulator.js
@@ -6,6 +6,7 @@ export class InvalidInputError extends Error {
 }
 
 export class Robot {
+  // Constants for cardinal directions and instructions
   static NORTH = "north";
   static EAST = "east";
   static SOUTH = "south";
@@ -17,30 +18,38 @@ export class Robot {
     R: "turnRight",
   };
 
+  // Static method to convert chain of instructions to instruction names
   static instructions(chain) {
     return [...chain].map((instruction) => Robot.INSTRUCTIONS[instruction]);
   }
 
+  // Private properties
   #bearing = Robot.NORTH;
   #coordinates;
 
+  // Getter for bearing
   get bearing() {
     return this.#bearing;
   }
 
+  // Getter for coordinates
   get coordinates() {
     return [...this.#coordinates];
   }
 
+  // Set the robot's orientation (bearing)
   orient(bearing) {
+    // Throw error for invalid bearing
     if (!Robot.BEARINGS.includes(bearing)) throw new InvalidInputError();
     this.#bearing = bearing;
   }
 
+  // Set the robot's initial coordinates
   at(...coordinates) {
     this.#coordinates = coordinates;
   }
 
+  // Turn the robot right based on current orientation
   turnRight() {
     this.orient(
       Robot.BEARINGS[
@@ -49,6 +58,7 @@ export class Robot {
     );
   }
 
+  // Turn the robot left based on current orientation
   turnLeft() {
     this.orient(
       Robot.BEARINGS[
@@ -57,6 +67,7 @@ export class Robot {
     );
   }
 
+  // Move the robot forward based on its orientation
   advance() {
     switch (this.bearing) {
       case Robot.NORTH:
@@ -70,11 +81,13 @@ export class Robot {
     }
   }
 
+  // Place the robot at a specific position and orientation
   place({ x, y, direction }) {
     this.orient(direction);
     this.at(x, y);
   }
 
+  // Evaluate a chain of instructions
   evaluate(chain) {
     Robot.instructions(chain).forEach((instruction) => this[instruction]());
   }
