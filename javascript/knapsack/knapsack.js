fa02bb6cb1e1bf26c86cc5bae146f82c7da98b05
Mike Goldenberg <csrutil@tuta.io>
2023-11-30 12:15:06 +0100

    knapsack js comments

--- javascript/knapsack/knapsack.js
+++ javascript/knapsack/knapsack.js
@@ -1,19 +1,27 @@
 export const knapsack = (maximumWeight, items) => {
+  // Get the number of items
   const n = items.length;
-  const dp = new Array(n + 1)
-    .fill(0)
-    .map(() => new Array(maximumWeight + 1).fill(0));
+  // Initialize a 2D array to store maximum values for different weights and items
+  const dp = new Array(n + 1).fill(0).map(() => new Array(maximumWeight + 1).fill(0));
 
+  // Loop through each item
   for (let i = 1; i <= n; i++) {
+    // Extract weight and value of the current item
     const { weight, value } = items[i - 1];
+
+    // Loop through each possible weight from 1 to maximumWeight
     for (let w = 1; w <= maximumWeight; w++) {
+      // If the current item's weight can be included
       if (weight <= w) {
+        // Choose the maximum value between including and excluding the current item
         dp[i][w] = Math.max(value + dp[i - 1][w - weight], dp[i - 1][w]);
       } else {
+        // If the current item's weight is more than the current capacity, exclude it
         dp[i][w] = dp[i - 1][w];
       }
     }
   }
 
+  // Return the maximum value achievable with the given maximum weight
   return dp[n][maximumWeight];
 };
