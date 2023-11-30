052308402fc12e40f567b30102c0d5694cfa745c
Mike Goldenberg <csrutil@tuta.io>
2023-11-30 12:02:20 +0100

    change js solution 2

--- javascript/change/change.js
+++ javascript/change/change.js
@@ -1,25 +1,48 @@
 export class Change {
   calculate(coinArray, target) {
+    if (target === 0) {
+      return []; // No change needed
+    }
+
     if (target < 0) {
       throw new Error("Negative totals are not allowed.");
     }
 
-    const sortedCoins = coinArray.sort((a, b) => a - b);
-    const change = [];
+    if (target < Math.min(...coinArray)) {
+      throw new Error(
+        "The total " + target + " cannot be represented in the given currency.",
+      );
+    }
 
-    let remaining = target;
-    for (const coin of sortedCoins) {
-      if (coin <= remaining) {
-        const count = Math.floor(remaining / coin);
-        remaining -= count * coin;
-        change.push(...Array(count).fill(coin));
+    const dp = new Array(target + 1).fill(Infinity);
+    dp[0] = 0;
+
+    for (const coin of coinArray) {
+      for (let i = coin; i <= target; i++) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
       }
     }
 
-    if (remaining !== 0) {
-      throw new Error("Cannot make change.");
+    if (dp[target] === Infinity) {
+      throw new Error(
+        "The total " + target + " cannot be represented in the given currency.",
+      );
     }
 
-    return change;
+    const coinsUsed = [];
+    let remaining = target;
+
+    while (remaining > 0) {
+      for (const coin of coinArray) {
+        if (remaining >= coin && dp[remaining - coin] + 1 === dp[remaining]) {
+          coinsUsed.push(coin);
+          remaining -= coin;
+          break;
+        }
+      }
+    }
+
+    return coinsUsed;
   }
 }
+
