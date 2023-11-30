59ac130e1675f8f421fd2c85ece10de7be2781f3
Mike Goldenberg <csrutil@tuta.io>
2023-11-30 14:43:00 +0100

    variable length quantity js comments

--- javascript/variable-length-quantity/variable-length-quantity.js
+++ javascript/variable-length-quantity/variable-length-quantity.js
@@ -1,15 +1,25 @@
 export const encode = (value) => {
   let result = [];
-
+  
+  // Loop until value is processed completely
   while (value > 0) {
-    let byte = value & 0x7f;
+    // Get the lowest 7 bits of the value
+    let byte = value & 0x7F;
 
-    if (value > 0x7f) byte |= 0x80;
+    // If there are more bits to encode, set the continuation bit (MSB)
+    if (value > 0x7F) byte |= 0x80;
+
+    // Add the byte to the result array
     result.push(byte);
+
+    // Shift value to the right by 7 bits
     value >>= 7;
   }
 
+  // If no bytes were added, the value is 0, so add a single zero byte
   if (result.length === 0) result.push(0);
+
+  // Return the array of bytes in reverse order
   return result.reverse();
 };
 
@@ -17,16 +27,22 @@ export const decode = (bytes) => {
   let result = 0;
   let shift = 0;
 
+  // Loop through each byte in the array
   for (let i = 0; i < bytes.length; i++) {
-    let byte = bytes[i] & 0x7f;
+    // Extract the lowest 7 bits from the byte
+    let byte = bytes[i] & 0x7F;
 
+    // Accumulate the byte's value into the result
     result |= byte << shift;
+
+    // Update the shift by 7 bits for the next iteration
     shift += 7;
 
+    // Check if the continuation bit (MSB) is not set
     if ((bytes[i] & 0x80) === 0) {
       return result;
     }
   }
 
-  throw new Error("Invalid variable length quantity");
+  throw new Error('Invalid variable length quantity');
 };
