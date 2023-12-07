f0b1551c6964e18f639469e95eae41b81c40af04
Mike Goldenberg <csrutil@tuta.io>
2023-12-07 20:55:10 +0100

    forth js solution

--- javascript/forth/forth.js
+++ javascript/forth/forth.js
@@ -1,18 +1,90 @@
-//
-// This is only a SKELETON file for the 'Forth' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Forth {
   constructor() {
-    throw Error('Remove this statement and implement this function');
+    this.stack = [];
+    this.defineWords = {};
   }
 
-  evaluate() {
-    throw Error('Remove this statement and implement this function');
+  evaluate(input) {
+    const tokens = input.split(' ');
+    for (let token of tokens) {
+      if (this.isNumber(token)) {
+        this.stack.push(parseInt(token));
+      } else if (this.isWord(token)) {
+        const word = token.toUpperCase();
+        if (this.defineWords[word]) {
+          const definition = this.defineWords[word];
+          const arguments = tokens.slice(1);
+          this.evalDef(definition, arguments);
+        } else if (word === ':') {
+          if (tokens.length !== 4) {
+            throw new Error('Invalid definition syntax');
+          }
+          const (name, definition, comment) = tokens.slice(1);
+          this.defineWords[name.toUpperCase()] = definition;
+          console.log(`Defined word: ${name}`);
+        } else {
+          throw new Error(`Unknown word: ${word}`);
+        }
+      } else {
+        throw new Error(`Invalid token: ${token}`);
+      }
+    }
   }
 
   get stack() {
-    throw Error('Remove this statement and implement this function');
+    return this.stack;
+  }
+
+  isNumber(token) {
+    return /^\d+$/.test(token);
+  }
+
+  isWord(token) {
+    return /^[a-zA-Z0-9_\-\+\*\/.\?]+$/.test(token);
+  }
+
+  evalDef(definition, arguments) {
+    const parseDef = this.parseExpression(definition);
+    const fn = (...args) => {
+      for (let arg of args) {
+        this.stack.push(arg);
+      }
+      const result = parseDef.reduce((acc, token) => {
+        switch (token) {
+          case '+':
+            return acc + this.stack.pop();
+          case '-':
+            return acc - this.stack.pop();
+          case '*':
+            return acc * this.stack.pop();
+          case '/':
+            if (this.stack.pop() === 0) {
+              throw new Error('Division by zero');
+            }
+            return acc / this.stack.pop();
+          case 'DUP':
+            return acc + acc;
+          case 'DROP':
+            return acc;
+          case 'SWAP':
+            const temp = this.stack.pop();
+            this.stack.push(acc);
+            return temp;
+          case 'OVER':
+            return this.stack[this.stack.length - 2] + acc;
+          default:
+            throw new Error(`Invalid token: ${token}`);
+        }
+      }, 0);
+      this.stack.pop();
+      this.stack.push(result);
+    };
+    for (let arg of arguments) {
+      fn(parseInt(arg));
+    }
+  }
+
+  parseExpression(expression) {
+    return expression.split(' ');
   }
 }
