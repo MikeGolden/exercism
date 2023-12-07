4bec009de7eb2d2ece8bd0817339f60424a0a172
Mike Goldenberg <csrutil@tuta.io>
2023-12-07 13:10:45 +0100

    all your base js comments

--- javascript/all-your-base/all-your-base.js
+++ javascript/all-your-base/all-your-base.js
@@ -1,16 +1,45 @@
+// Declare the `convert` function that takes an array of digits, the source base, and the target base as parameters
 export const convert = (digits, fromBase, toBase) => {
-  if (fromBase < 2) throw new Error('Wrong input base');
-  if (toBase < 2) throw new Error('Wrong output base');
-  if (digits.length === 1 && digits[0]*digits[0] === digits[0]) return digits;
-  if (digits.length === 0 || digits[0] === 0 || digits.some(d => d < 0 || d >= fromBase))
+
+  // Validate the source base
+  if (fromBase < 2) {
+    // Throw an error if the source base is less than 2
+    throw new Error('Wrong input base');
+  }
+
+  // Validate the target base
+  if (toBase < 2) {
+    // Throw an error if the target base is less than 2
+    throw new Error('Wrong output base');
+  }
+
+  // Handle the special case where the number is 1
+  if (digits.length === 1 && digits[0] * digits[0] === digits[0]) {
+    // If the number is 1, return the array
+    return digits;
+  }
+
+  // Check for invalid input
+  if (digits.length === 0 || digits[0] === 0 || digits.some(d => d < 0 || d >= fromBase)) {
+    // Throw an error if the input array is empty, starts with 0, or contains invalid digits
     throw new Error('Input has wrong format');
+  }
 
-  let value = digits.reduce((acc, d) => acc*fromBase + d, 0);
+  // Convert the number from the source base to the target base
+  let value = digits.reduce((acc, d) => acc * fromBase + d, 0);
 
+  // Create an array to store the resulting digits in the target base
   const result = [];
+
+  // Iterate until the value is non-zero
   while (value) {
+    // Add the remainder of the division by the target base to the result array
     result.unshift(value % toBase);
+
+    // Truncate the value to the quotient of the division by the target base
     value = Math.floor(value / toBase);
   }
+
+  // Return the array of converted digits in the target base
   return result;
 };
