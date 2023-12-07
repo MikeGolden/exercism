5019eac8618ba66a69384525ad155e69b2c02512
Mike Goldenberg <csrutil@tuta.io>
2023-12-07 20:57:01 +0100

    forth js solution 2

--- javascript/forth/forth.js
+++ javascript/forth/forth.js
@@ -1,90 +1,95 @@
+const OPERATION_MAP = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => {
+    if (b === 0) throw Error("Division by zero");
+    return Math.floor(a / b);
+  },
+};
+const OPERATORS = new Set(Object.keys(OPERATION_MAP));
+const getVars = (stack, count = 1) => {
+  if (stack.length < count) throw Error("Stack empty");
+  return stack.splice(count * -1);
+};
+const COMMAND_MAP = {
+  dup: (stack) => {
+    const [a] = getVars(stack);
+    stack.push(a);
+    stack.push(a);
+  },
+  drop: (stack) => getVars(stack),
+  swap: (stack) => {
+    const [a, b] = getVars(stack, 2);
+    stack.push(b, a);
+  },
+  over: (stack) => {
+    const [a, b] = getVars(stack, 2);
+    stack.push(a, b, a);
+  },
+};
+const STACK_COMMANDS = new Set(Object.keys(COMMAND_MAP));
+
 export class Forth {
   constructor() {
-    this.stack = [];
-    this.defineWords = {};
+    this._stack = [];
+    this._dictionary = {};
   }
 
-  evaluate(input) {
-    const tokens = input.split(' ');
-    for (let token of tokens) {
-      if (this.isNumber(token)) {
-        this.stack.push(parseInt(token));
-      } else if (this.isWord(token)) {
-        const word = token.toUpperCase();
-        if (this.defineWords[word]) {
-          const definition = this.defineWords[word];
-          const arguments = tokens.slice(1);
-          this.evalDef(definition, arguments);
-        } else if (word === ':') {
-          if (tokens.length !== 4) {
-            throw new Error('Invalid definition syntax');
-          }
-          const (name, definition, comment) = tokens.slice(1);
-          this.defineWords[name.toUpperCase()] = definition;
-          console.log(`Defined word: ${name}`);
-        } else {
-          throw new Error(`Unknown word: ${word}`);
-        }
+  _defineCommand(terms) {
+    const command = terms[1];
+
+    if (command.match(/^-?\d+$/)) throw Error("Invalid definition");
+
+    const action = this._expandActions(terms.slice(2, -1)).join(" ");
+
+    this._dictionary[command] = action;
+  }
+
+  _expandActions(terms) {
+    return terms.flatMap((term) => this._dictionary[term]?.split(" ") ?? term);
+  }
+
+  _performArithmetic(operator) {
+    const b = this._stack.pop();
+    const a = this._stack.pop();
+
+    if (a === undefined || b === undefined) throw Error("Stack empty");
+
+    const result = OPERATION_MAP[operator](a, b);
+    this._stack.push(result);
+  }
+
+  _manipulateStack(command) {
+    COMMAND_MAP[command](this._stack);
+  }
+
+  _executeActions(actions) {
+    actions.forEach((action) => {
+      if (OPERATORS.has(action)) {
+        this._performArithmetic(action);
+      } else if (STACK_COMMANDS.has(action)) {
+        this._manipulateStack(action);
+      } else if (action.match(/^-?\d+$/)) {
+        this._stack.push(Number(action));
       } else {
-        throw new Error(`Invalid token: ${token}`);
+        throw Error("Unknown command");
       }
+    });
+  }
+
+  evaluate(rawInput) {
+    const inputTerms = rawInput.toLowerCase().split(" ");
+
+    if (inputTerms[0] === ":") {
+      this._defineCommand(inputTerms);
+      return;
     }
+
+    this._executeActions(this._expandActions(inputTerms));
   }
 
   get stack() {
-    return this.stack;
-  }
-
-  isNumber(token) {
-    return /^\d+$/.test(token);
-  }
-
-  isWord(token) {
-    return /^[a-zA-Z0-9_\-\+\*\/.\?]+$/.test(token);
-  }
-
-  evalDef(definition, arguments) {
-    const parseDef = this.parseExpression(definition);
-    const fn = (...args) => {
-      for (let arg of args) {
-        this.stack.push(arg);
-      }
-      const result = parseDef.reduce((acc, token) => {
-        switch (token) {
-          case '+':
-            return acc + this.stack.pop();
-          case '-':
-            return acc - this.stack.pop();
-          case '*':
-            return acc * this.stack.pop();
-          case '/':
-            if (this.stack.pop() === 0) {
-              throw new Error('Division by zero');
-            }
-            return acc / this.stack.pop();
-          case 'DUP':
-            return acc + acc;
-          case 'DROP':
-            return acc;
-          case 'SWAP':
-            const temp = this.stack.pop();
-            this.stack.push(acc);
-            return temp;
-          case 'OVER':
-            return this.stack[this.stack.length - 2] + acc;
-          default:
-            throw new Error(`Invalid token: ${token}`);
-        }
-      }, 0);
-      this.stack.pop();
-      this.stack.push(result);
-    };
-    for (let arg of arguments) {
-      fn(parseInt(arg));
-    }
-  }
-
-  parseExpression(expression) {
-    return expression.split(' ');
+    return this._stack;
   }
 }
