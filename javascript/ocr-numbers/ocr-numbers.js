0e60bf539dedf474205f3847e01f21e01840cc2c
Mike Goldenberg <csrutil@tuta.io>
2023-12-08 10:21:33 +0100

    ocr numbers js solution 2

--- javascript/ocr-numbers/ocr-numbers.js
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -1,48 +1,43 @@
-export const convert = (input) => {
-  const binaryFont = {
-    " _ | ||_|": "0",
-    "     |  |": "1",
-    " _  _||_ ": "2",
-    " _  _| _|": "3",
-    "   |_|  |": "4",
-    " _ |_  _|": "5",
-    " _ |_ |_|": "6",
-    " _   |  |": "7",
-    " _ |_||_|": "8",
-    " _ |_| _|": "9",
-  };
-
-  const lines = input.split("\n");
-  if (lines.length % 4 !== 0 || lines[0].length % 3 !== 0) {
-    throw new Error("Incorrect input size");
-  }
-
-  const convertBlock = (block) => {
-    if (binaryFont.hasOwnProperty(block)) {
-      return binaryFont[block];
-    }
-    return "?";
-  };
-
-  const convertToDigits = (blocks) => {
-    const blocksArray = blocks.match(/.{3}/g);
-    const digits = [];
-    for (let i = 0; i < blocksArray.length; i += 3) {
-      const digitBlock = blocksArray.slice(i, i + 3).join("\n");
-      const digit = convertBlock(digitBlock);
-      digits.push(digit);
-    }
-    return digits.join("");
-  };
-
-  const blockRows = lines.reduce((acc, line, index) => {
-    const blockIndex = Math.floor(index / 4);
-    if (!acc[blockIndex]) {
-      acc[blockIndex] = "";
-    }
-    acc[blockIndex] += line;
-    return acc;
-  }, []);
-
-  return blockRows.map(convertToDigits).join(",");
+const ocrFont = {
+  " _ | ||_|": "0",
+  "     |  |": "1",
+  " _  _||_ ": "2",
+  " _  _| _|": "3",
+  "   |_|  |": "4",
+  " _ |_  _|": "5",
+  " _ |_ |_|": "6",
+  " _   |  |": "7",
+  " _ |_||_|": "8",
+  " _ |_| _|": "9",
 };
+function extractNums([line1, line2, line3]) {
+  if (
+    (line1.length + line2.length + line3.length) / 3 !== line1.length ||
+    line1.length % 3 !== 0
+  ) {
+    throw new Error("Invalid character size detected");
+  }
+  const nums = [];
+  for (let i = 0; i < line1.length; i += 3) {
+    const num = `${line1.slice(i, i + 3)}${line2.slice(i, i + 3)}${line3.slice(
+      i,
+      i + 3
+    )}`;
+    nums.push(num in ocrFont ? ocrFont[num] : "?");
+  }
+  return nums;
+}
+export function convert(ocr) {
+  return ocr
+    .split("\n")
+    .flatMap((_, i, rows) => {
+      if (i % 4 === 0) {
+        const lines = rows.slice(i, i + 3);
+        const nums = extractNums(lines);
+        return [nums.join("")];
+      } else {
+        return [];
+      }
+    })
+    .join(",");
+}
