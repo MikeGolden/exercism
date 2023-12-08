1bad6152d1a0fd08c852acd55f5a9149e4e52ab8
Mike Goldenberg <csrutil@tuta.io>
2023-12-08 10:20:07 +0100

    ocr numbers js solution

--- javascript/ocr-numbers/ocr-numbers.js
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -1,8 +1,48 @@
-//
-// This is only a SKELETON file for the 'OCR Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const convert = (input) => {
+  const binaryFont = {
+    " _ | ||_|": "0",
+    "     |  |": "1",
+    " _  _||_ ": "2",
+    " _  _| _|": "3",
+    "   |_|  |": "4",
+    " _ |_  _|": "5",
+    " _ |_ |_|": "6",
+    " _   |  |": "7",
+    " _ |_||_|": "8",
+    " _ |_| _|": "9",
+  };
 
-export const convert = () => {
-  throw new Error('Remove this statement and implement this function');
+  const lines = input.split("\n");
+  if (lines.length % 4 !== 0 || lines[0].length % 3 !== 0) {
+    throw new Error("Incorrect input size");
+  }
+
+  const convertBlock = (block) => {
+    if (binaryFont.hasOwnProperty(block)) {
+      return binaryFont[block];
+    }
+    return "?";
+  };
+
+  const convertToDigits = (blocks) => {
+    const blocksArray = blocks.match(/.{3}/g);
+    const digits = [];
+    for (let i = 0; i < blocksArray.length; i += 3) {
+      const digitBlock = blocksArray.slice(i, i + 3).join("\n");
+      const digit = convertBlock(digitBlock);
+      digits.push(digit);
+    }
+    return digits.join("");
+  };
+
+  const blockRows = lines.reduce((acc, line, index) => {
+    const blockIndex = Math.floor(index / 4);
+    if (!acc[blockIndex]) {
+      acc[blockIndex] = "";
+    }
+    acc[blockIndex] += line;
+    return acc;
+  }, []);
+
+  return blockRows.map(convertToDigits).join(",");
 };
