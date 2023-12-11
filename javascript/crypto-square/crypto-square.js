080b31ebd572be6a856a5c023c429800c17a5289
Mike Goldenberg <csrutil@tuta.io>
2023-12-11 13:38:03 +0100

    crypto square js solution

--- javascript/crypto-square/crypto-square.js
+++ javascript/crypto-square/crypto-square.js
@@ -1,14 +1,42 @@
-//
-// This is only a SKELETON file for the 'Crypto Square' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Crypto {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(text) {
+    this.normalizedText = this.normalize(text);
+    this.size = this.calculateSize(this.normalizedText.length);
+  }
+
+  normalize(text) {
+    return text.replace(/[^a-zA-Z]/g, "").toLowerCase();
+  }
+
+  calculateSize(length) {
+    const size = Math.sqrt(length);
+    const rows = Math.floor(size);
+    const columns = Math.ceil(size);
+
+    if (rows * columns >= length) {
+      return { rows, columns };
+    } else {
+      return { rows: columns, columns };
+    }
   }
 
   get ciphertext() {
-    throw new Error('Remove this statement and implement this function');
+    const { rows, columns } = this.size;
+    let encoded = "";
+
+    for (let col = 0; col < columns; col++) {
+      for (let row = 0; row < rows; row++) {
+        const index = col + row * columns;
+        if (index < this.normalizedText.length) {
+          encoded += this.normalizedText[index];
+        } else {
+          encoded += " ";
+        }
+      }
+      if (col !== columns - 1) {
+        encoded += " ";
+      }
+    }
+    return encoded;
   }
 }
