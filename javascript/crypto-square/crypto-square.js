808ccd5dd4944aa27ffec707e75799f0bfdf43f7
Mike Goldenberg <csrutil@tuta.io>
2023-12-11 13:40:26 +0100

    crypto square js comments

--- javascript/crypto-square/crypto-square.js
+++ javascript/crypto-square/crypto-square.js
@@ -1,11 +1,20 @@
 export class Crypto {
   constructor(plaintext) {
-    const lowertext = plaintext.toLowerCase().replace(/\W/g, '')
-    const length = Math.ceil(Math.sqrt(lowertext.length))
+    // Convert plaintext to lowercase and remove non-alphabetic characters
+    const lowertext = plaintext.toLowerCase().replace(/\W/g, '');
+    
+    // Calculate the size of the square code (length of the plaintext)
+    const length = Math.ceil(Math.sqrt(lowertext.length));
+
+    // Create the ciphertext by arranging characters into a square code
     this.ciphertext = 
+      // Generate an array of rows
       Array.from({ length }, (_, i) => 
+        // For each row, create an array of columns
         Array.from({ length }, (_, j) =>
+          // Fill each column with characters from the plaintext (or space if no character)
           lowertext[length * j + i] || ' ').join(''))
-      .join(' ')
+      // Join rows with spaces to form the final ciphertext
+      .join(' ');
   }
 }
