6b6b715325ca575e0cb677157845b81d9f3523e6
Mike Goldenberg <csrutil@tuta.io>
2023-12-11 13:40:10 +0100

    crypto square js solution 2

--- javascript/crypto-square/crypto-square.js
+++ javascript/crypto-square/crypto-square.js
@@ -1,42 +1,11 @@
 export class Crypto {
-  constructor(text) {
-    this.normalizedText = this.normalize(text);
-    this.size = this.calculateSize(this.normalizedText.length);
-  }
-
-  normalize(text) {
-    return text.replace(/[^a-zA-Z]/g, "").toLowerCase();
-  }
-
-  calculateSize(length) {
-    const size = Math.sqrt(length);
-    const rows = Math.floor(size);
-    const columns = Math.ceil(size);
-
-    if (rows * columns >= length) {
-      return { rows, columns };
-    } else {
-      return { rows: columns, columns };
-    }
-  }
-
-  get ciphertext() {
-    const { rows, columns } = this.size;
-    let encoded = "";
-
-    for (let col = 0; col < columns; col++) {
-      for (let row = 0; row < rows; row++) {
-        const index = col + row * columns;
-        if (index < this.normalizedText.length) {
-          encoded += this.normalizedText[index];
-        } else {
-          encoded += " ";
-        }
-      }
-      if (col !== columns - 1) {
-        encoded += " ";
-      }
-    }
-    return encoded;
+  constructor(plaintext) {
+    const lowertext = plaintext.toLowerCase().replace(/\W/g, '')
+    const length = Math.ceil(Math.sqrt(lowertext.length))
+    this.ciphertext = 
+      Array.from({ length }, (_, i) => 
+        Array.from({ length }, (_, j) =>
+          lowertext[length * j + i] || ' ').join(''))
+      .join(' ')
   }
 }
