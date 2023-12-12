df89ada3cfc653d5c90722627fb689dcedc8f1cc
Mike Goldenberg <csrutil@tuta.io>
2023-12-12 13:07:05 +0100

    saddle points js solutioin

--- javascript/saddle-points/saddle-points.js
+++ javascript/saddle-points/saddle-points.js
@@ -1,8 +1,30 @@
-//
-// This is only a SKELETON file for the 'Saddle Points' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const saddlePoints = (matrix) => {
+  const saddlePoint = [];
 
-export const saddlePoints = () => {
-  throw new Error('Remove this statement and implement this function');
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      const currentHeight = matrix[row][col];
+      let isSaddlePoint = true;
+
+      for (let i = 0; i < matrix[0].length; i++) {
+        if (matrix[row][i] > currentHeight) {
+          isSaddlePoint = false;
+          break;
+        }
+      }
+
+      for (let j = 0; j < matrix.length; j++) {
+        if (matrix[j][col] < currentHeight) {
+          isSaddlePoint = false;
+          break;
+        }
+      }
+
+      if (isSaddlePoint) {
+        saddlePoint.push([row, col]);
+      }
+    }
+  }
+
+  return saddlePoint;
 };
