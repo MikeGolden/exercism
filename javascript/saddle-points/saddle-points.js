3f90670ee39f6e53679405d4f7b7d57484a0b4c6
Mike Goldenberg <csrutil@tuta.io>
2023-12-12 13:08:40 +0100

    saddle points js solutiion 2

--- javascript/saddle-points/saddle-points.js
+++ javascript/saddle-points/saddle-points.js
@@ -1,34 +1,19 @@
 export const saddlePoints = (matrix) => {
-  const saddlePoints = [];
+  let rows = matrix;
+  let columns = transpose(matrix);
 
-  // Iterate through each cell of the matrix
-  for (let row = 0; row < matrix.length; row++) {
-    for (let col = 0; col < matrix[0].length; col++) {
-      const currentHeight = matrix[row][col];
-      let isSaddlePoint = true;
+  let rowsMaximums = rows.map((row) => Math.max(...row));
+  let columnMinimums = columns.map((column) => Math.min(...column));
 
-      // Check if current cell is the largest in its row
-      for (let i = 0; i < matrix[0].length; i++) {
-        if (matrix[row][i] > currentHeight) {
-          isSaddlePoint = false;
-          break;
-        }
-      }
-
-      // Check if current cell is the smallest in its column
-      for (let j = 0; j < matrix.length; j++) {
-        if (matrix[j][col] < currentHeight) {
-          isSaddlePoint = false;
-          break;
-        }
-      }
-
-      // If current cell meets criteria, add as a potential saddle point
-      if (isSaddlePoint) {
-        saddlePoints.push([row, col]);
-      }
-    }
-  }
-
-  return saddlePoints;
+  return rows.flatMap((row, x) =>
+    row.flatMap((point, y) =>
+      point === rowsMaximums[x] && point === columnMinimums[y]
+        ? { row: x + 1, column: y + 1 }
+        : []
+    )
+  );
 };
+
+function transpose(matrix) {
+  return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
+}
