7395ac7f4f5ad1263d7d9611dc6289c67f1d3654
Mike Goldenberg <csrutil@tuta.io>
2023-12-12 12:53:59 +0100

    sum of multiples js comments

--- javascript/sum-of-multiples/sum-of-multiples.js
+++ javascript/sum-of-multiples/sum-of-multiples.js
@@ -2,14 +2,17 @@ export const sum = (level, magicalItems) => {
   let points = 0;
   const multiples = new Set();
 
-  magicalItems.forEach((item) => {
+  // Iterate through each magical item
+  magicalItems.forEach(item => {
     const baseValue = item;
 
+    // Find all multiples of the base value less than the level
     for (let i = baseValue; i < level; i += baseValue) {
-      multiples.add(i);
+      multiples.add(i); // Add unique multiples to a Set
     }
   });
 
+  // Sum all unique multiples in the Set
   for (let num of multiples) {
     points += num;
   }
