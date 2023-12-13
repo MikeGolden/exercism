62ea820bf657912416005ef6088428addbc07445
Mike Goldenberg <csrutil@tuta.io>
2023-12-13 13:13:59 +0100

    food chain js solution

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,14 +1,33 @@
-//
-// This is only a SKELETON file for the 'Food Chain' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Song {
-  verse() {
-    throw new Error('Remove this statement and implement this function');
+  verse(num) {
+    const verses = [
+      "I know an old lady who swallowed a fly.",
+      "I don't know why she swallowed the fly. Perhaps she'll die.",
+      "I know an old lady who swallowed a spider.",
+      "It wriggled and jiggled and tickled inside her.",
+      "She swallowed the spider to catch the fly.",
+      "I don't know why she swallowed the fly. Perhaps she'll die.",
+      "I know an old lady who swallowed a bird.",
+      "How absurd to swallow a bird!",
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+      "She swallowed the spider to catch the fly.",
+      "I don't know why she swallowed the fly. Perhaps she'll die.",
+      "I know an old lady who swallowed a cat.",
+      "Imagine that, to swallow a cat!",
+      "She swallowed the cat to catch the bird.",
+    ];
+
+    return verses[num - 1];
   }
 
-  verses() {
-    throw new Error('Remove this statement and implement this function');
+  verses(startVerse, endVerse) {
+    let song = "";
+    for (let i = startVerse; i <= endVerse; i++) {
+      song += this.verse(i);
+      if (i !== endVerse) {
+        song += "\n";
+      }
+    }
+    return song;
   }
 }
