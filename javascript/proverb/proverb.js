f7c9ee680a979009e304b4f5bd3dfd8c51130d34
Mike Goldenberg <csrutil@tuta.io>
2023-12-13 13:34:57 +0100

    proverb js solution

--- javascript/proverb/proverb.js
+++ javascript/proverb/proverb.js
@@ -1,8 +1,17 @@
-//
-// This is only a SKELETON file for the 'Proverb' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const proverb = (...items) => {
+  const qualifier =
+    typeof items[items.length - 1] === "object"
+      ? items.pop().qualifier + " "
+      : "";
+  const lines = [];
 
-export const proverb = () => {
-  throw new Error('Remove this statement and implement this function');
+  for (let i = 0; i < items.length - 1; i++) {
+    lines.push(`For want of a ${items[i]} the ${items[i + 1]} was lost.`);
+  }
+
+  if (items.length > 0) {
+    lines.push(`And all for the want of a ${qualifier}${items[0]}.`);
+  }
+
+  return lines.join("\n");
 };
