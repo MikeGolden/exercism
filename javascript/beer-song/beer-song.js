ae51a6a919f5e789497e56146b0c24f5e693df25
Mike Goldenberg <csrutil@tuta.io>
2023-12-14 12:33:03 +0100

    beer js solution

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,8 +1,35 @@
-//
-// This is only a SKELETON file for the 'Beer Song' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const recite = (initialBottlesCount, takeDownCount) => {
-  throw new Error('Remove this statement and implement this function');
+  let lyrics = [];
+
+  while (initialBottlesCount >= takeDownCount) {
+    lyrics.push(
+      `${initialBottlesCount} bottles of beer on the wall, ${initialBottlesCount} bottles of beer.`,
+    );
+    initialBottlesCount -= takeDownCount;
+
+    const remaining = initialBottlesCount === 1 ? "bottle" : "bottles";
+    const nextCount =
+      initialBottlesCount > takeDownCount ? takeDownCount : initialBottlesCount;
+
+    if (initialBottlesCount > 0) {
+      lyrics.push(
+        `Take ${nextCount} down and pass it around, ${initialBottlesCount} ${remaining} of beer on the wall.`,
+      );
+    } else {
+      lyrics.push(
+        "Take one down and pass it around, no more bottles of beer on the wall.",
+      );
+    }
+  }
+
+  if (initialBottlesCount === 0) {
+    lyrics.push(
+      "No more bottles of beer on the wall, no more bottles of beer.",
+    );
+    lyrics.push(
+      "Go to the store and buy some more, 99 bottles of beer on the wall.",
+    );
+  }
+
+  return lyrics;
 };
