c3508928446256c37d091a53382f56b8b30cf43c
Mike Goldenberg <csrutil@tuta.io>
2023-12-15 12:35:03 +0100

    two bucket js solution

--- javascript/two-bucket/two-bucket.js
+++ javascript/two-bucket/two-bucket.js
@@ -1,14 +1,70 @@
-//
-// This is only a SKELETON file for the 'Two Bucket' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class TwoBucket {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(bucketOneSize, bucketTwoSize) {
+    this.bucketOneSize = bucketOneSize;
+    this.bucketTwoSize = bucketTwoSize;
+    this.bucketOne = 0;
+    this.bucketTwo = 0;
   }
 
-  solve() {
-    throw new Error('Remove this statement and implement this function');
+  solve(desiredAmount, startBucket) {
+    if (
+      desiredAmount < 0 ||
+      desiredAmount > this.bucketOneSize + this.bucketTwoSize
+    ) {
+      return {
+        moves: -1,
+        goalBucket: null,
+        otherBucket: null,
+      };
+    }
+
+    if (startBucket !== "one" && startBucket !== "two") {
+      return {
+        moves: -1,
+        goalBucket: null,
+        otherBucket: null,
+      };
+    }
+
+    let moves = 0;
+    if (startBucket === "one") {
+      this.bucketOne = this.bucketOneSize;
+      moves++;
+    } else {
+      this.bucketTwo = this.bucketTwoSize;
+      moves++;
+    }
+
+    while (
+      this.bucketOne !== desiredAmount &&
+      this.bucketTwo !== desiredAmount
+    ) {
+      if (this.bucketOne === this.bucketOneSize) {
+        this.pourWater(this.bucketOne, this.bucketTwo);
+        moves++;
+      } else if (this.bucketTwo === this.bucketTwoSize) {
+        this.pourWater(this.bucketTwo, this.bucketOne);
+        moves++;
+      } else {
+        this.pourWater(this.bucketOne, this.bucketTwo);
+        moves++;
+      }
+    }
+
+    return {
+      moves: moves,
+      goalBucket: this.bucketOne === desiredAmount ? "one" : "two",
+      otherBucket:
+        this.bucketOne === desiredAmount ? this.bucketTwo : this.bucketOne,
+    };
+  }
+
+  pourWater(sourceBucket, destinationBucket) {
+    const amountToPour = Math.min(
+      this[sourceBucket],
+      this[destinationBucket] - this[destinationBucket],
+    );
+    this[sourceBucket] -= amountToPour;
+    this[destinationBucket] += amountToPour;
   }
 }
