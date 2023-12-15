20b79df821b9bd5e5f79ab60177d91e390a53159
Mike Goldenberg <csrutil@tuta.io>
2023-12-15 12:37:10 +0100

    two bucket js solution 2

--- javascript/two-bucket/two-bucket.js
+++ javascript/two-bucket/two-bucket.js
@@ -1,78 +1,62 @@
 export class TwoBucket {
-  constructor(bucketOneSize, bucketTwoSize) {
-    this.bucketOneSize = bucketOneSize;
-    this.bucketTwoSize = bucketTwoSize;
-    this.bucketOne = 0;
-    this.bucketTwo = 0;
+  constructor(sizeOf1, sizeOf2, goal, starter) {
+    this.#validate(sizeOf1, sizeOf2, goal);
+    this.sizeOf1 = sizeOf1;
+    this.sizeOf2 = sizeOf2;
+    this.goal = goal;
+    this.start = starter;
   }
 
-  solve(desiredAmount, startBucket) {
-    // Check if the desired amount is valid.
-    if (desiredAmount < 0 || desiredAmount > this.bucketOneSize + this.bucketTwoSize) {
-      return {
-        moves: -1,
-        goalBucket: null,
-        otherBucket: null
-      };
-    }
+  #validate(one, two, goal) {
+    const gcd = (a, b) => (!b ? a : gcd(b, a % b));
+    if (goal % gcd(one, two)) throw new Error();
+    if (goal > one && goal > two) throw new Error();
+  }
 
-    // Check if the starting bucket is valid.
-    if (startBucket !== 'one' && startBucket !== 'two') {
-      return {
-        moves: -1,
-        goalBucket: null,
-        otherBucket: null
-      };
-    }
-
-    // Initialize the moves variable.
+  solve() {
     let moves = 0;
-
-    // Start filling the starting bucket.
-    if (startBucket === 'one') {
-      this.bucketOne = this.bucketOneSize;
-      moves++;
-    } else {
-      this.bucketTwo = this.bucketTwoSize;
-      moves++;
+    let one = new Bucket("one", this.sizeOf1, 0);
+    let two = new Bucket("two", this.sizeOf2, 0);
+    if (this.start === "two") {
+      [one, two] = [two, one];
     }
 
-    // While the desired amount has not been reached, continue pouring water between the buckets.
-    while (this.bucketOne !== desiredAmount && this.bucketTwo !== desiredAmount) {
-
-      // If the starting bucket is full, pour water into the other bucket.
-      if (this.bucketOne === this.bucketOneSize) {
-        this.pourWater(this.bucketOne, this.bucketTwo);
-        moves++;
+    while (true) {
+      switch (true) {
+        case one.content === this.goal || two.content === this.goal:
+          if (two.content === this.goal) {
+            [one, two] = [two, one];
+          }
+          return {
+            moves,
+            goalBucket: one.name,
+            otherBucket: two.content,
+          };
+        case one.content === 0:
+          one.content = one.size;
+          break;
+        case two.size === this.goal:
+          two.content = two.size;
+          break;
+        case two.content === two.size:
+          two.content = 0;
+          break;
+        default:
+          const pour = Math.min(one.content, two.size - two.content);
+          one.content -= pour;
+          two.content += pour;
+          break;
       }
-
-      // If the other bucket is full, pour water into the starting bucket.
-      else if (this.bucketTwo === this.bucketTwoSize) {
-        this.pourWater(this.bucketTwo, this.bucketOne);
-        moves++;
-      }
-
-      // If neither bucket is full, pour water from the starting bucket into the other bucket.
-      else {
-        this.pourWater(this.bucketOne, this.bucketTwo);
-        moves++;
-      }
+      moves++;
     }
-
-    // Return the solution object.
-    return {
-      moves: moves,
-      goalBucket: this.bucketOne === desiredAmount ? 'one' : 'two',
-      otherBucket: this.bucketOne === desiredAmount ? this.bucketTwo : this.bucketOne
-    };
-  }
-
-  pourWater(sourceBucket, destinationBucket) {
-    // Get the amount of water that can be poured from the source bucket to the destination bucket.
-    const amountToPour = Math.min(this[sourceBucket], this[destinationBucket] - this[destinationBucket]);
-
-    // Pour the water from the source bucket to the destination bucket.
-    this[sourceBucket] -= amountToPour;
-    this[destinationBucket] += amountToPour;
   }
 }
+
+class Bucket {
+  constructor(name, size, content) {
+    this.name = name;
+    this.size = size;
+    this.content = content;
+  }
+}
+
