eba9b54f42206f616e1abcdba639c24457a6b2d7
Mike Goldenberg <csrutil@tuta.io>
2023-12-15 12:35:20 +0100

    two bucket js comments

--- javascript/two-bucket/two-bucket.js
+++ javascript/two-bucket/two-bucket.js
@@ -7,27 +7,29 @@ export class TwoBucket {
   }
 
   solve(desiredAmount, startBucket) {
-    if (
-      desiredAmount < 0 ||
-      desiredAmount > this.bucketOneSize + this.bucketTwoSize
-    ) {
+    // Check if the desired amount is valid.
+    if (desiredAmount < 0 || desiredAmount > this.bucketOneSize + this.bucketTwoSize) {
       return {
         moves: -1,
         goalBucket: null,
-        otherBucket: null,
+        otherBucket: null
       };
     }
 
-    if (startBucket !== "one" && startBucket !== "two") {
+    // Check if the starting bucket is valid.
+    if (startBucket !== 'one' && startBucket !== 'two') {
       return {
         moves: -1,
         goalBucket: null,
-        otherBucket: null,
+        otherBucket: null
       };
     }
 
+    // Initialize the moves variable.
     let moves = 0;
-    if (startBucket === "one") {
+
+    // Start filling the starting bucket.
+    if (startBucket === 'one') {
       this.bucketOne = this.bucketOneSize;
       moves++;
     } else {
@@ -35,35 +37,41 @@ export class TwoBucket {
       moves++;
     }
 
-    while (
-      this.bucketOne !== desiredAmount &&
-      this.bucketTwo !== desiredAmount
-    ) {
+    // While the desired amount has not been reached, continue pouring water between the buckets.
+    while (this.bucketOne !== desiredAmount && this.bucketTwo !== desiredAmount) {
+
+      // If the starting bucket is full, pour water into the other bucket.
       if (this.bucketOne === this.bucketOneSize) {
         this.pourWater(this.bucketOne, this.bucketTwo);
         moves++;
-      } else if (this.bucketTwo === this.bucketTwoSize) {
+      }
+
+      // If the other bucket is full, pour water into the starting bucket.
+      else if (this.bucketTwo === this.bucketTwoSize) {
         this.pourWater(this.bucketTwo, this.bucketOne);
         moves++;
-      } else {
+      }
+
+      // If neither bucket is full, pour water from the starting bucket into the other bucket.
+      else {
         this.pourWater(this.bucketOne, this.bucketTwo);
         moves++;
       }
     }
 
+    // Return the solution object.
     return {
       moves: moves,
-      goalBucket: this.bucketOne === desiredAmount ? "one" : "two",
-      otherBucket:
-        this.bucketOne === desiredAmount ? this.bucketTwo : this.bucketOne,
+      goalBucket: this.bucketOne === desiredAmount ? 'one' : 'two',
+      otherBucket: this.bucketOne === desiredAmount ? this.bucketTwo : this.bucketOne
     };
   }
 
   pourWater(sourceBucket, destinationBucket) {
-    const amountToPour = Math.min(
-      this[sourceBucket],
-      this[destinationBucket] - this[destinationBucket],
-    );
+    // Get the amount of water that can be poured from the source bucket to the destination bucket.
+    const amountToPour = Math.min(this[sourceBucket], this[destinationBucket] - this[destinationBucket]);
+
+    // Pour the water from the source bucket to the destination bucket.
     this[sourceBucket] -= amountToPour;
     this[destinationBucket] += amountToPour;
   }
