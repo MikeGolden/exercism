6232099802e17d3a8a0fd5f1b4f45041a20d4c13
Mike Goldenberg <csrutil@tuta.io>
2023-12-19 14:40:23 +0100

    kindergarten garden js comments

--- javascript/kindergarten-garden/kindergarten-garden.js
+++ javascript/kindergarten-garden/kindergarten-garden.js
@@ -22,23 +22,34 @@ const PLANT_CODES = {
 
 export class Garden {
   constructor(diagram, students = DEFAULT_STUDENTS) {
+    // Split the diagram into individual rows
     const rows = diagram.split("\n");
 
+    // Create an empty object to store plants for each student
     this.plantsMap = {};
+
+    // Sort students' names alphabetically
     students.sort();
+
+    // Split the diagram into pairs of plants for each row and assign to each student
     for (let i = 0; i < students.length; i++) {
       const student = students[i];
       const start = i * 2;
       const end = start + 2;
+
+      // Extract the plants for the current student
       const plants = [
         ...rows[0].slice(start, end),
         ...rows[1].slice(start, end),
       ];
+
+      // Map the plant codes to plant names and assign to the current student
       this.plantsMap[student] = plants.map((code) => PLANT_CODES[code]);
     }
   }
 
   plants(student) {
+    // Return the plants for the specified student
     return this.plantsMap[student];
   }
 }
