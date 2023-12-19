f161b15b022604ea05ac3daac504bdef0c3888fb
Mike Goldenberg <csrutil@tuta.io>
2023-12-19 14:11:15 +0100

    bowling js solution

--- javascript/bowling/bowling.js
+++ javascript/bowling/bowling.js
@@ -1,14 +1,38 @@
-//
-// This is only a SKELETON file for the 'Bowling' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Bowling {
-  roll() {
-    throw new Error('Remove this statement and implement this function');
+  constructor() {
+    this.rolls = [];
+  }
+
+  roll(pins) {
+    this.rolls.push(pins);
   }
 
   score() {
-    throw new Error('Remove this statement and implement this function');
+    let totalScore = 0;
+    let rollIndex = 0;
+
+    const isStrike = () => this.rolls[rollIndex] === 10;
+    const strikeBonus = () =>
+      this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+    const isSpare = () =>
+      this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10;
+    const spareBonus = () => this.rolls[rollIndex + 2];
+    const sumOfPinsInFrame = () =>
+      this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+
+    for (let frame = 0; frame < 10; frame++) {
+      if (isStrike()) {
+        totalScore += 10 + strikeBonus();
+        rollIndex++;
+      } else if (isSpare()) {
+        totalScore += 10 + spareBonus();
+        rollIndex += 2;
+      } else {
+        totalScore += sumOfPinsInFrame();
+        rollIndex += 2;
+      }
+    }
+
+    return totalScore;
   }
 }
