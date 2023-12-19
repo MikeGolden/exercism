a01681901b87a360c896f4fb2c7579b435a0be29
Mike Goldenberg <csrutil@tuta.io>
2023-12-19 14:39:49 +0100

    kindergarten garden js solution

--- javascript/kindergarten-garden/kindergarten-garden.js
+++ javascript/kindergarten-garden/kindergarten-garden.js
@@ -1,36 +1,44 @@
-//
-// This is only a SKELETON file for the 'Kindergarten Garden' exercise.
-// It's been provided as a convenience to get you started writing code faster.
-//
-
 const DEFAULT_STUDENTS = [
-  'Alice',
-  'Bob',
-  'Charlie',
-  'David',
-  'Eve',
-  'Fred',
-  'Ginny',
-  'Harriet',
-  'Ileana',
-  'Joseph',
-  'Kincaid',
-  'Larry',
+  "Alice",
+  "Bob",
+  "Charlie",
+  "David",
+  "Eve",
+  "Fred",
+  "Ginny",
+  "Harriet",
+  "Ileana",
+  "Joseph",
+  "Kincaid",
+  "Larry",
 ];
 
 const PLANT_CODES = {
-  G: 'grass',
-  V: 'violets',
-  R: 'radishes',
-  C: 'clover',
+  G: "grass",
+  V: "violets",
+  R: "radishes",
+  C: "clover",
 };
 
 export class Garden {
   constructor(diagram, students = DEFAULT_STUDENTS) {
-    throw new Error('Remove this statement and implement this function');
+    const rows = diagram.split("\n");
+
+    this.plantsMap = {};
+    students.sort();
+    for (let i = 0; i < students.length; i++) {
+      const student = students[i];
+      const start = i * 2;
+      const end = start + 2;
+      const plants = [
+        ...rows[0].slice(start, end),
+        ...rows[1].slice(start, end),
+      ];
+      this.plantsMap[student] = plants.map((code) => PLANT_CODES[code]);
+    }
   }
 
   plants(student) {
-    throw new Error('Remove this statement and implement this function');
+    return this.plantsMap[student];
   }
 }
