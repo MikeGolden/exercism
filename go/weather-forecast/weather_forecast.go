951636def3fc62e6fc624146bde6b223bf1d7c8c
Mike Goldenberg <csrutil@tuta.io>
2023-12-20 12:33:45 +0100

    weather forecast go solution

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather represents weather forecast.
 package weather
 
+// CurrentCondition is one variable.
 var CurrentCondition string
+
+// CurrentLocation variable.
 var CurrentLocation string
 
+// Forecast function is a weather forecast function.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
