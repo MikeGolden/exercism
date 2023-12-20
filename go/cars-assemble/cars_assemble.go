deb8de43e1d5271b52d44628781f07c37c408ae3
Mike Goldenberg <csrutil@tuta.io>
2023-12-20 12:28:13 +0100

    cars assemble go solution 2

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -1,27 +1,18 @@
 package cars
 
-import "math"
-
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	successfulCarsPerHour := float64(productionRate) * (successRate / 100.0)
-	return successfulCarsPerHour
+	return float64(productionRate) * (successRate / 100)
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	successfulCarsPerHour := CalculateWorkingCarsPerHour(productionRate, successRate)
-	successfulCarsPerMinute := int(math.Round(successfulCarsPerHour / 60.0))
-	return successfulCarsPerMinute
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	groupsOfTen := carsCount / 10
-	individualCars := carsCount % 10
-
-	totalCost := uint(groupsOfTen*95000 + individualCars*10000)
-	return totalCost
+	return uint(95000 * (carsCount / 10) + 10000 * (carsCount % 10))
 }
