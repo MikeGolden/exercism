d0c787c58065a0edab2b7ab652406e35fd4136d8
Mike Goldenberg <csrutil@tuta.io>
2023-12-20 12:53:23 +0100

    welcome to tech palace go solution

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,26 @@
 package techpalace
 
+import (
+	"fmt"
+	"strings"
+)
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	return fmt.Sprintf("Welcome to the Tech Palace, %s", strings.ToUpper(customer))
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	return fmt.Sprintf("%s\n%s\n%s", border(numStarsPerLine), welcomeMsg, border(numStarsPerLine))
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	lines := strings.Split(oldMsg, "\n")
+	return strings.Trim(lines[1], "* ")
+}
+
+func border(numStarsPerLine int) string {
+	return strings.Repeat("*", numStarsPerLine)
 }
