c2c06342deb28edcac20489b614326bc97a1058b
Mike Goldenberg <csrutil@tuta.io>
2023-12-27 13:33:22 +0100

    blackjack go solution 2

--- go/blackjack/blackjack.go
+++ go/blackjack/blackjack.go
@@ -2,51 +2,55 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	switch card {
-	case "ace":
-		return 11
-	case "two":
-		return 2
-	case "three":
-		return 3
-	case "four":
-		return 4
-	case "five":
-		return 5
-	case "six":
-		return 6
-	case "seven":
-		return 7
-	case "eight":
-		return 8
-	case "nine":
-		return 9
-	case "ten", "jack", "queen", "king":
-		return 10
-	default:
-		return 0 // or any value you wish to denote an invalid card
-	}
+	switch {
+        case card == "ten" || card == "jack" || card == "queen" || card == "king":
+            	return 10
+        case card == "ace":
+    			return 11
+        case card == "two":
+    			return 2
+        case card == "three":
+    			return 3
+        case card == "four":
+    			return 4
+        case card == "five":
+    			return 5
+        case card == "six":
+    			return 6
+        case card == "seven":
+    			return 7
+        case card == "eight":
+    			return 8
+        case card == "nine":
+    			return 9
+        default:
+    			return 0
+    }
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	value1 := ParseCard(card1)
-	value2 := ParseCard(card2)
-	dealerValue := ParseCard(dealerCard)
-
-	total := value1 + value2
-
-	// Decisioin Logic
-	if total == 21 {
-		return "P" // Blackjack!
-	} else if value1 == 11 && value2 == 11 {
-		return "P" // Two aces, always split
-	} else if total >= 17 {
-		return "S" // Stand
-	} else if total >= 13 && total <= 16 && dealerValue <= 6 {
-		return "S" // Stand
-	} else {
-		return "H" // Hit
-	}
+    var totalCardValue int
+    totalCardValue = ParseCard(card1) + ParseCard(card2)
+	switch {
+        case card1 == "ace" && card2 == "ace":
+    		return "P"
+        case totalCardValue == 21:
+    		if dealerCard != "ace" && ParseCard(dealerCard) != 10{
+            	return "W"    
+            } else {
+            	return "S"
+            }
+		case totalCardValue >= 17 && totalCardValue <= 20:
+        	return "S"
+		case totalCardValue >= 12 && totalCardValue <= 16 && ParseCard(dealerCard) < 7:
+        	return "S"
+		case totalCardValue >= 12 && totalCardValue <= 16 && ParseCard(dealerCard) >= 7:
+        	return "H"
+        case totalCardValue <= 11:
+        	return "H"
+		default:
+        	return "S"
+    }
 }
